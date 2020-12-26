add flicker by hours shaded map; fix normalization to be by how many selected intervals rather than entire year

hybrid/flicker/flicker_mismatch.py
@@ -141,11 +141,10 @@ class FlickerMismatch:
         :param solar_resource_data: If not provided, try to load file from 'resource_files' directory
         :return:
         """
-        n_steps = 8760 * self.steps_per_hour
         step_to_minute = int(60 / self.steps_per_hour)
         self.azi_ang, self.elv_ang, _ = get_sun_pos(self.lat,
                                                     self.lon,
-                                                    n_steps,
+                                                    self.n_steps,
                                                     step_to_minute)
 
         pv_model = pv.default("PVWattsNone")
@@ -420,19 +419,19 @@ class FlickerMismatch:
 
         by_poa = by_power = by_time = False
 
-        if "poa" in weight_option:
-            by_poa = True
-            total_poa = sum(self.poa)
-            heat_map_shadow = copy.deepcopy(self.heat_map_template[0])
-
-        if "power" in weight_option:
-            by_power = True
-            heat_map_flicker = copy.deepcopy(self.heat_map_template[0])
-
-        if "time" in weight_option:
-            by_time = True
-            time_weight = 1 / self.n_steps
-            heat_map_time = copy.deepcopy(self.heat_map_template[0])
+        for i in weight_option:
+            if i == "poa":
+                by_poa = True
+                total_poa = sum(self.poa[steps])
+                heat_map_shadow = copy.deepcopy(self.heat_map_template[0])
+            elif i == "power":
+                by_power = True
+                heat_map_flicker = copy.deepcopy(self.heat_map_template[0])
+            elif i == "time":
+                by_time = True
+                heat_map_time = copy.deepcopy(self.heat_map_template[0])
+            else:
+                raise ValueError("Unrecognized 'weight_option'")
 
         if not (by_poa or by_power or by_time):
             raise ValueError("No valid 'weight_option' provided. Provide a list of selected ways to weight the shading "
@@ -451,7 +450,7 @@ class FlickerMismatch:
                 continue
 
             if by_poa:
-                poa_weight = self.poa[hr] / total_poa / self.steps_per_hour
+                poa_weight = self.poa[hr] / total_poa
                 FlickerMismatch._calculate_shading(poa_weight, shadows, self.site_points, heat_map_shadow)
 
             if by_power:
@@ -459,16 +458,24 @@ class FlickerMismatch:
                                                       self.array_string_points, heat_map_flicker)
 
             if by_time:
-                FlickerMismatch._calculate_shading(time_weight, shadows, self.site_points, heat_map_time)
+                FlickerMismatch._calculate_shading(1, shadows, self.site_points, heat_map_time)
+
+        # normalize by angles per hour (since each will use the same weight) or by number of hours total
+        step_normalize = self.angles_per_step if self.angles_per_step else 1
+        if by_poa:
+            heat_map_shadow /= step_normalize
+        if by_power:
+            heat_map_flicker /= step_normalize * len(steps)
+        if by_time:
+            heat_map_time /= step_normalize * len(steps)
 
         heat_maps_to_return = []
-
         for i in weight_option:
             if i == 'poa':
                 heat_maps_to_return.append(heat_map_shadow)
             elif i == 'power':
                 heat_maps_to_return.append(heat_map_flicker)
-            else:
+            elif i == 'time':
                 heat_maps_to_return.append(heat_map_time)
 
         logger.info("Finished heat maps")
@@ -489,15 +496,6 @@ class FlickerMismatch:
         :param intervals: list of ranges to simulate; if none, simulate entire weather file's records
         :return: heat_map_shadow, heat_map_flicker
         """
-        heat_map_shadow_path = (Path(__file__).parent / "data" /
-                                str(self.filename_base + "_shadow.txt"))
-        heat_map_flicker_path = (Path(__file__).parent / "data" /
-                                 str(self.filename_base + "_flicker.txt"))
-
-        if heat_map_flicker_path.is_file() and heat_map_shadow_path.is_file():
-            logger.info("loaded heat maps from file")
-            # return np.loadtxt(heat_map_shadow_path), np.loadtxt(heat_map_flicker_path)
-
         logger.info("run_parallel with {} processes".format(n_procs))
         pool = self._create_pool(n_procs)
         if intervals is None:
@@ -505,27 +503,26 @@ class FlickerMismatch:
         results = pool.imap(functools.partial(self.create_heat_maps, weight_option=weight_option),
                             intervals)
 
-        # shadow calculations
-        heat_map_shadow = copy.deepcopy(self.heat_map_template[0])
+        # aggregate results and renormalize
+        heat_maps_to_return = [copy.deepcopy(self.heat_map_template[0]) for n in weight_option]
 
-        # flicker calculations
-        heat_map_flicker = copy.deepcopy(self.heat_map_template[0])
+        total_steps = sum([len(i) for i in intervals])
+        total_poa = sum([self.poa[i] for i in intervals])
+        for r, i in zip(results, intervals):
+            for j, hm in enumerate(heat_maps_to_return):
+                if weight_option[j] == 'poa':
+                    hm += r[j] * sum(self.poa[i]) / total_poa
+                elif weight_option[j] == 'power' or weight_option[j] == 'time':
+                    hm += r[j] * len(i) / total_steps
 
-        for r in results:
-            heat_map_shadow += r[0]
-            heat_map_flicker += r[1]
+        logger.info("Create_heat_map success")
 
-        # normalize
-        heat_map_shadow /= self.angles_per_step
-        heat_map_flicker /= self.angles_per_step * self.steps_per_hour
-
-        logger.info("Create_heat_map_irradiance success")
-
-        return heat_map_shadow, heat_map_flicker
+        return tuple(heat_maps_to_return)
 
     def plot_on_site(self,
-                     plot_points=True,
-                     plot_array=True):
+                     plot_array=True,
+                     plot_points=True
+                     ):
         fig, axs = plt.subplots()
         axs.set_aspect('equal')
         xs, ys = self.site.exterior.xy
@@ -536,4 +533,10 @@ class FlickerMismatch:
             for p in self.array:
                 x, y = p.exterior.xy
                 plt.plot(x, y)
+        if plot_points:
+            for p in self.array_string_points:
+                for s in p:
+                    xs = [point.x for point in s]
+                    ys = [point.y for point in s]
+                    plt.scatter(xs, ys)
         return axs

tests/hybrid/test_flicker_mismatch.py
@@ -1,105 +1,116 @@
 from pytest import approx
-import time
 from hybrid.flicker.data.plot_flicker import *
 
 lat = 39.7555
 lon = -105.2211
 
 
+def plot_maps(maps, flicker):
+    for m in maps:
+        axs = flicker.plot_on_site(False, False)
+        c = plot_contour(m, flicker, axs, vmin=np.amin(m), vmax=np.amax(m))
+        plt.colorbar(c)
+        plt.show()
+
+
 def test_single_turbine():
     FlickerMismatch.diam_mult_nwe = 3
     FlickerMismatch.diam_mult_s = 1
     flicker = FlickerMismatch(lat, lon, angles_per_step=1)
-    shadow, loss = flicker.create_heat_maps(range(3186, 3200), ("poa", "power"))
+    shadow, loss = flicker.create_heat_maps(range(3185, 3187), ("poa", "power"))
 
-    assert(np.max(shadow) == approx(0.00067, 1e-1))
-    assert(np.average(shadow) * 1e6 == approx(3.1, 1e-1))
-    assert(np.count_nonzero(shadow) > 900)
-    assert(np.max(loss) > 0.2)
-    assert(np.average(loss) > 9e-6)
-    assert(np.count_nonzero(loss) == 60)
+    assert(np.max(shadow) == 1.0)
+    assert(np.average(shadow) == approx(0.0041092, 1e-4))
+    assert(np.count_nonzero(shadow) == 636)
+    assert(np.max(loss) == approx(0.314133, 1e-4))
+    assert(np.average(loss) == approx(0.0042872, 1e-4))
+    assert(np.count_nonzero(loss) == 2940)
 
-    axs = flicker.plot_on_site(False, False)
-    plot_contour(loss, flicker, axs)
-    # plt.xlim((-30, 30))
-    # plt.ylim((40, 100))
-    # plt.show()
+    # run parallel
+    shadow_p, loss_p = flicker.run_parallel(2, (range(3185, 3186), range(3186, 3187)), ("poa", "power"))
+
+    assert(np.max(shadow_p) == 1.0)
+    assert(np.average(shadow_p) == approx(0.0041092, 1e-4))
+    assert(np.count_nonzero(shadow_p) == 636)
+    assert(np.max(loss_p) == approx(0.314133, 1e-4))
+    assert(np.average(loss_p) == approx(0.0042872, 1e-4))
+    assert(np.count_nonzero(loss_p) == 2940)
+
+    # plot_maps((shadow, loss), flicker)
 
 
-def test_parallel_single():
+def test_single_turbine_multiple_angles():
     FlickerMismatch.diam_mult_nwe = 3
     FlickerMismatch.diam_mult_s = 1
-    flicker = FlickerMismatch(lat, lon, angles_per_step=1)
+    flicker = FlickerMismatch(lat, lon, angles_per_step=3)
+    shadow, loss = flicker.create_heat_maps(range(3185, 3187), ("poa", "power"))
 
-    shadow, loss = flicker.run_parallel(2, (range(3186, 3188), range(3188, 3200)), ("poa", "power"))
+    assert(np.max(shadow) == approx(1.0, 1e-4))
+    assert(np.average(shadow) == approx(0.0042229, 1e-4))
+    assert(np.count_nonzero(shadow) == 698)
+    assert(np.max(loss) == approx(0.314133, 1e-4))
+    assert(np.average(loss) == approx(0.0043571, 1e-4))
+    assert(np.count_nonzero(loss) == 3010)
 
-    assert(np.max(shadow) == approx(0.00067, 1e-1))
-    assert(np.average(shadow) * 1e6 == approx(3.1, 1e-1))
-    assert(np.count_nonzero(shadow) > 900)
-    assert(np.max(loss) > 0.2)
-    assert(np.average(loss) > 9e-6)
-    assert(np.count_nonzero(loss) == 60)
+    # run parallel
+    shadow_p, loss_p = flicker.run_parallel(2, (range(3185, 3186), range(3186, 3187)), ("poa", "power"))
+
+    assert(np.max(shadow_p) == approx(1.0, 1e-4))
+    assert(np.average(shadow_p) == approx(0.0042229, 1e-4))
+    assert(np.count_nonzero(shadow_p) == 698)
+    assert(np.max(loss_p) == approx(0.314133, 1e-4))
+    assert(np.average(loss_p) == approx(0.0043571, 1e-4))
+    assert(np.count_nonzero(loss_p) == 3010)
+
+    # plot_maps((shadow, loss), flicker)
 
 
 def test_single_turbine_time_weighted():
+    # two time steps: one with shading, one without
     FlickerMismatch.diam_mult_nwe = 3
     FlickerMismatch.diam_mult_s = 1
     flicker = FlickerMismatch(lat, lon, angles_per_step=None)
-    (hours_shaded, ) = flicker.create_heat_maps(range(3186, 3208), ("time",))
+    (hours_shaded, ) = flicker.create_heat_maps(range(3187, 3189), ("time",))
 
-    axs = flicker.plot_on_site(False, False)
-    c = plot_contour(hours_shaded, flicker, axs, vmin=np.amin(hours_shaded), vmax=np.amax(hours_shaded))
-    # plt.colorbar(c)
-    # plt.show()
-    assert(np.max(hours_shaded) == approx(0.000913242))
-    assert(np.average(hours_shaded) == approx(1.206235e-05))
-    assert(np.count_nonzero(hours_shaded) > 6560)
+    intervals = (range(3187, 3188), range(3188, 3189))
+    (hours_shaded_p, ) = flicker.run_parallel(2, intervals, ("time",))
 
+    assert(np.max(hours_shaded) == approx(0.5))
+    assert(np.average(hours_shaded) == approx(0.0015781, 1e-4))
+    assert(np.count_nonzero(hours_shaded) == 251)
 
-dx = 1
-dy = 2
-angle = 0
+    assert(np.max(hours_shaded_p) == approx(0.5))
+    assert(np.average(hours_shaded_p) == approx(0.0015781, 1e-4))
+    assert(np.count_nonzero(hours_shaded_p) == 251)
+
+    # plot_maps((hours_shaded, hours_shaded_p), flicker)
 
 
 def test_grid():
+    dx = 1
+    dy = 2
+    angle = 0
+    flicker = FlickerMismatchGrid(lat, lon, dx, dy, angle, angles_per_step=1)
+    shadow, loss = flicker.create_heat_maps(range(3185, 3187), ("poa", "power"))
+
+    assert(np.max(shadow) == 1.0)
+    assert(np.average(shadow) == approx(0.031547, 1e-4))
+    assert(np.count_nonzero(shadow) == approx(390))
+    assert(np.max(loss) == approx(0.418338, 1e-4))
+    assert(np.average(loss) == approx(0.033167, 1e-4))
+    assert(np.count_nonzero(loss) == 1364)
+
+    # run parallel with  multiple angles
     flicker = FlickerMismatchGrid(lat, lon, dx, dy, angle, angles_per_step=3)
-    shadow, loss = flicker.create_heat_maps(range(3186, 3200), ("poa", "power"))
-    axs = flicker.plot_on_site()
+    intervals = (range(3185, 3186), range(3186, 3187))
+    shadow_p, loss_p = flicker.run_parallel(2, intervals, ("poa", "power"))
 
-    assert(np.max(shadow) == approx(0.0021, 1e-1))
-    assert(np.average(shadow) == approx(7.5e-05, 1e-1))
-    assert(np.count_nonzero(shadow) == approx(750, 1e-1))
-    assert(np.max(loss) == approx(3.4, 1e-1))
-    assert(np.count_nonzero(loss) == approx(2170, 1e-2))
-
-    plot_contour(shadow, flicker, axs)
-    # plt.show()
-
-
-def test_parallel_grid():
-    flicker = FlickerMismatchGrid(lat, lon, dx, dy, angle)
-
-    start = time.time()
-    shadow_s, flicker_map_s = flicker.create_heat_maps(range(500, 560), ("poa", "power"))
-    print("serial time:", time.time() - start)
-
-    start = time.time()
-    intervals = (range(500, 530), range(530, 560))
-    shadow_p, flicker_map_p = flicker.run_parallel(2, intervals, ("poa", "power"))
-    print("2 proc time:", time.time() - start)
-
-    start = time.time()
-    intervals = (range(500, 510), range(510, 520), range(520, 530), range(530, 540), range(540, 550), range(550, 560))
-    shadow_p, flicker_map_p = flicker.run_parallel(6, intervals, ("poa", "power"))
-    print("6 proc time:", time.time() - start)
-
-    start = time.time()
-    intervals = (range(500, 505), ) * 12
-    shadow_p, flicker_map_p = flicker.run_parallel(12, intervals, ("poa", "power"))
-    print("12 proc time:", time.time() - start)
-
-    diff_shadow = shadow_p - shadow_s
-    diff_flicker = flicker_map_p - flicker_map_s
+    assert(np.max(shadow_p) == 1.0)
+    assert(np.average(shadow_p) == approx(0.031462, 1e-4))
+    assert(np.count_nonzero(shadow_p) == approx(390))
+    assert(np.max(loss_p) == approx(0.41833, 1e-4))
+    assert(np.average(loss_p) == approx(0.0331158, 1e-4))
+    assert(np.count_nonzero(loss_p) == 1364)
 
 
 def test_plot():
@@ -114,5 +125,4 @@ def test_plot():
 
     flicker = FlickerMismatch(lat, lon, angles_per_step=12)
     axs = flicker.plot_on_site(False, False)
-    plot_contour(flicker_heatmap, flicker, axs)
     plot_tiled(flicker_heatmap, flicker, axs)