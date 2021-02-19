9e9ab1deb99932dc143c6b05e0124b2b3caaec68
King <jennifer.king@nrel.gov>
2021-02-19 15:22:15 -0700

    Changed the name so it is less confusing

--- examples/simple_battery_dispatch.py
+++ examples/simple_battery_dispatch.py
@@ -20,7 +20,7 @@ def simple_dispatch(combined_pv_wind_curtailment_hopp,energy_shortfall_hopp,N):
     # storage module
     rated_size = 4000 # kW -> 200 MW
     charge_rate = 4000 # kWH -> 200 MWh
-    battery_generation = np.zeros(N)
+    battery_SOC = np.zeros(N)
     battery_used = np.zeros(N)
     excess_energy = np.zeros(N)
     for i in range(1,N):
@@ -28,30 +28,30 @@ def simple_dispatch(combined_pv_wind_curtailment_hopp,energy_shortfall_hopp,N):
         # should you charge
         if combined_pv_wind_curtailment_hopp['0'].iloc[i] > 0:
             if i == 0:
-                battery_generation = np.min([combined_pv_wind_curtailment_hopp['0'].iloc[i], charge_rate])
+                battery_SOC = np.min([combined_pv_wind_curtailment_hopp['0'].iloc[i], charge_rate])
             else:
-                battery_generation[i] = battery_generation[i - 1]
-            if battery_generation[i-1] < rated_size:
+                battery_SOC[i] = battery_SOC[i - 1]
+            if battery_SOC[i-1] < rated_size:
                 add_gen = np.min([combined_pv_wind_curtailment_hopp['0'].iloc[i], charge_rate])
-                battery_generation[i] = np.min([battery_generation[i] + add_gen, rated_size])
+                battery_SOC[i] = np.min([battery_SOC[i] + add_gen, rated_size])
         else:
-            battery_generation[i] = battery_generation[i - 1]
+            battery_SOC[i] = battery_SOC[i - 1]
 
         # should you discharge
-        if energy_shortfall_hopp['0'].iloc[i] > 0 and battery_generation[i] > 0:
-            energy_used = np.min([energy_shortfall_hopp['0'].iloc[i], battery_generation[i]])
-            battery_generation[i] = battery_generation[i] - energy_used
+        if energy_shortfall_hopp['0'].iloc[i] > 0 and battery_SOC[i] > 0:
+            energy_used = np.min([energy_shortfall_hopp['0'].iloc[i], battery_SOC[i]])
+            battery_SOC[i] = battery_SOC[i] - energy_used
             battery_used[i] = energy_used
 
         # overall the amount of energy you could have sold to the grid assuming perfect knowledge
-        if battery_generation[i] == rated_size and battery_generation[i-1] == rated_size:
+        if battery_SOC[i] == rated_size and battery_SOC[i-1] == rated_size:
             excess_energy[i] = combined_pv_wind_curtailment_hopp['0'].iloc[i]
         else:
-            excess_energy[i] = np.max([combined_pv_wind_curtailment_hopp['0'].iloc[i] - battery_generation[i], 0.0])
+            excess_energy[i] = np.max([combined_pv_wind_curtailment_hopp['0'].iloc[i] - battery_SOC[i], 0.0])
 
-    return battery_used, excess_energy, battery_generation
+    return battery_used, excess_energy, battery_SOC
 
-battery_used, excess_energy, battery_generation = simple_dispatch(combined_pv_wind_curtailment_hopp,energy_shortfall_hopp,N)
+battery_used, excess_energy, battery_SOC = simple_dispatch(combined_pv_wind_curtailment_hopp,energy_shortfall_hopp,N)
 
 print('Battery Generation: ', np.sum(battery_used))
 print('Amount of energy going to the grid: ', np.sum(excess_energy))
@@ -61,7 +61,7 @@ plt.figure()
 plt.plot(combined_pv_wind_curtailment_hopp['0'].iloc[0:N], label='Available Curtailed Power')
 plt.plot(excess_energy,label='Excess Energy')
 plt.plot(energy_shortfall_hopp['0'].iloc[0:N], label='Energy Shortfall')
-plt.plot(battery_generation,label='Battery Charge')
+plt.plot(battery_SOC,label='Battery Charge')
 plt.plot(battery_used,label='Battery Discharge')
 plt.xlabel('Time (hr)',fontsize=15)
 plt.ylabel('Power (kW)', fontsize=15)
