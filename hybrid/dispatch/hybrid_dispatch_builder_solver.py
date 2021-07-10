393fa38f97c85b7c27d1910ac21f24d46710e3a0
dguittet <dguittet@nrel.gov>
2021-07-10 19:18:39 +0200

    turn off print statement

--- hybrid/dispatch/hybrid_dispatch_builder_solver.py
+++ hybrid/dispatch/hybrid_dispatch_builder_solver.py
@@ -179,7 +179,6 @@ class HybridDispatchBuilderSolver:
         # Solving the year in series
         ti = list(range(0, self.site.n_timesteps, self.options.n_roll_periods))
         for i, t in enumerate(ti):
-            print('Evaluating period ', i, ' out of ', len(ti))
             self.simulate_with_dispatch(t)
             if self.options.is_test and i > 10:
                 break
