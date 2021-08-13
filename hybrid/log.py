28af53c0efeb1b14e650eead61729b8dd66700b0
dguittet <dguittet@nrel.gov>
2021-08-13 09:01:17 -0600

    fix logging

--- hybrid/log.py
+++ hybrid/log.py
@@ -7,15 +7,18 @@ from pathlib import Path
 # set to logging.WARNING for fewer messages
 logging_level = logging.INFO
 
+
 # set up logging to file - see previous section for more details
 cluster = os.environ.get('SLURM_CLUSTER_NAME')
 
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 if cluster == "eagle":
     print("logging to stdout")
     logging.basicConfig(level=logging_level,
-                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                         datefmt='%m-%d %H:%M',
                         stream=sys.stdout)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
 else:
     run_suffix = '_' + datetime.now().isoformat().replace(':', '.')
     log_path = Path.cwd() / "log"
@@ -23,28 +26,30 @@ else:
         os.mkdir(log_path)
     log_path = log_path / ("hybrid_systems" + run_suffix + ".log")
     print(log_path)
-    logging.basicConfig(level=logging_level,
-                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                        datefmt='%m-%d %H:%M',
-                        filename=str(log_path),
-                        filemode='w')
+    # logging.basicConfig(level=logging_level,
+    #                     datefmt='%m-%d %H:%M',
+    #                     filename=str(log_path),
+    #                     filemode='w')
+    handler = logging.FileHandler(str(log_path))
+    handler.setFormatter(formatter)
 
 # define a Handler which writes WARNING messages or higher to the sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.WARNING)
-# set a format which is simpler for console use
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-# tell the handler to use this format
-console.setFormatter(formatter)
-# add the handler to the root logger
 logging.getLogger('').addHandler(console)
 
 # Now, define a couple of other loggers which might represent areas in your
 # application:
 
 hybrid_logger = logging.getLogger('HybridSim')
-flicker_logger = logging.getLogger('Flicker')
+flicker_logger = hybrid_logger
+bos_logger = hybrid_logger
+analysis_logger = hybrid_logger
 opt_logger = logging.getLogger('Optimization')
-bos_logger = logging.getLogger('BOSCost')
-analysis_logger = logging.getLogger('Analysis')
 
+hybrid_logger.addHandler(handler)
+opt_logger.addHandler(handler)
+
+logging.getLogger('').propagate = False
+logging.getLogger('HybridSim').propagate = False
+logging.getLogger('Optimization').propagate = False
