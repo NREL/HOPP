fc042641f034b32f83b2fe0aadbad44d6af15639
dguittet <dguittet@nrel.gov>
2021-10-02 07:54:34 -0600

    fix files

--- hybrid/keys.py
+++ hybrid/keys.py
@@ -11,11 +11,11 @@ def set_developer_nrel_gov_key(key: str):
 
 def get_developer_nrel_gov_key():
     global developer_nrel_gov_key
-    if len(developer_nrel_gov_key) != 40:
+    if developer_nrel_gov_key is None or len(developer_nrel_gov_key) != 40:
         raise ValueError("Please provide NREL Developer key using `set_developer_nrel_gov_key`"
                          "(`from hybrid.keys import set_developer_nrel_gov_key`) \n"
-                         " - or ensure your Developer key is set using the .env file method."
-                         " For details on how to do this, "
+                         " Ensure your Developer key is set either as a `NREL_API_KEY` Environment Variable or"
+                         " using the .env file method. For details on how to do this, "
                          "please see Section 7 and 8 of Readme.md")
     return developer_nrel_gov_key
 
@@ -26,4 +26,5 @@ def set_nrel_key_dot_env(path=None):
     else:
         r = load_dotenv()
     NREL_API_KEY = os.getenv("NREL_API_KEY")
-    set_developer_nrel_gov_key(NREL_API_KEY)
+    if NREL_API_KEY is not None:
+        set_developer_nrel_gov_key(NREL_API_KEY)
