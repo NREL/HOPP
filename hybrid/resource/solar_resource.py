b935980c463de21022c77c72089b7e330203b775
jmartin4nrel <jonathan.martin@nrel.gov>
2022-04-14 11:12:20 -0600

    V2 of API download protocol (V1 deprecated)

--- hybrid/resource/solar_resource.py
+++ hybrid/resource/solar_resource.py
@@ -52,7 +52,7 @@ class SolarResource(Resource):
         logger.info("SolarResource: {}".format(self.filename))
 
     def download_resource(self):
-        url = 'https://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?wkt=POINT({lon}+{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(
+        url = 'https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-download.csv?wkt=POINT({lon}+{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(
             year=self.year, lat=self.latitude, lon=self.longitude, leap=self.leap_year, interval=self.interval,
             utc=self.utc, name=self.name, email=self.email,
             mailing_list=self.mailing_list, affiliation=self.affiliation, reason=self.reason, api=get_developer_nrel_gov_key(),
