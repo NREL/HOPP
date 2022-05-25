bbd48475921386cbe184f51d6adb1326b77b4a91
kbrunik <kbrunik@gmail.com>
2022-05-25 06:24:51 -0700

    Added static 50MW 6hr storage pressure vessel.

--- examples/H2_Analysis/pressure_vessel_model.py
+++ examples/H2_Analysis/pressure_vessel_model.py
@@ -20,13 +20,45 @@ Lined-rock Cavern:
 - More expensive than salt cavern
 - CAPEX and OPEX decrease with economy of scale
 """
+from os import stat
 import CoolProp.CoolProp as CP
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+def static_pressure_vessel(H2_storage):
+    """Nominal storage volume is 300 MWh (50 MW, 6 hours)
+    https://www.nrel.gov/docs/fy10osti/48360.pdf
+    Assume 85% compressor efficiency
+    Assume 60% Fuel cell conversion
+    Assume 20 degrees celcius, isothermal compression
+    Assume compressed to 100 bar
+    """
+    
+    if H2_storage == True:
+        #Round trip storage (PEM -> Compressor -> Storage -> Fuel Cell): 31250 kg H2
+        storage_volume = 4007.9665  #[m^3]
+        compressor_work = 380.9388  #[kWh]
+        pressure_vessel_capex = 17500000    #[USD]
+        pressure_vessel_opex = 2625000  #[USD/yr]
+        print("H2 Storage volume: ",storage_volume, "[m^3]",\
+            "Compressor Work: ", compressor_work, "[kWh]", "Pressure Vessel Capex: ",\
+                pressure_vessel_capex, "[USD]", "Pressure Vessel Opex: ", \
+                    pressure_vessel_opex, "[USD/yr]")
+    else:
+        pass
+    return 
 
-def pressure_vessel(amount_hydrogen,temperature, pressure, storage_capacity = 500000):
+        # #Storage (PEM -> Compressor -> Storage): 18750 kg H2
+        # storage_volume = 2404.7799  #[m^3]
+        # compressor_work = 228.5632  #[kWh]
+        # pressure_vessel_capex = 10500000    #[USD]
+        # pressure_vessel_opex = 1575000  #[USD/yr]
+
+
+
+
+def pressure_vessel(amount_hydrogen,temperature, pressure):
     """Inputs: amount_hydrogen [kg], temperature [celcius], pressure [bar], 
     plant storage_capacity [kg]
     https://www.sciencedirect.com/science/article/pii/S0360319921005838#tbl2"""
@@ -39,13 +71,12 @@ def pressure_vessel(amount_hydrogen,temperature, pressure, storage_capacity = 50
 
     #Calculate compressibility factor at given temp and pressure
     Z = CP.PropsSI('Z', 'P', P_given, 'T', T_given, fluid) 
-
+  
     #Calculate volumetric storage density
     v = ((1/n)*Z*R*T_given) / P_given    #[m^3/kg]
 
     #Calculate required storage volume of hydrogen
     storage_volume = v * amount_hydrogen    #[m^3]
-    
 
     #Compressor Work required
     """Assume 85% compressor efficiency. Initial state of hydrogen is 
@@ -57,10 +88,9 @@ def pressure_vessel(amount_hydrogen,temperature, pressure, storage_capacity = 50
     #Hydrogen's enthalpy before compression [J/kg]
     P_initial = 2e6 #[Pa] Typical outlet pressure of PEM electrolyzer
     h_initial = CP.PropsSI('H','P', P_initial, 'T', T_given, fluid)  #[J/kg]
-
+    
     #Hydrogen's enthaply after compression [J/kg]   
     h_final = CP.PropsSI('H','P', P_given, 'T', T_given, fluid) #[J/kg]
-    
 
     w = (h_final - h_initial) / comp_efficiency #[J/kg] 
 
@@ -78,6 +108,9 @@ def pressure_vessel(amount_hydrogen,temperature, pressure, storage_capacity = 50
     return storage_volume, compressor_work, pressure_vessel_capex, pressure_vessel_opex
 
 
+if __name__ == '__main__':
+    test=pressure_vessel(18750,20,100)
+    print(test)
 
-
-
+    static_pressure_vessel(True)
+    
