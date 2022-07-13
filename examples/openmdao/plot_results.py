fe7f2861cb80a69ca79859f690fb658eb8e6d3bf
dakotaramos <ramos.dakotasky@gmail.com>
2022-07-13 08:55:15 -0600

    updated plot file and standardized the generic optimization run file

---
+++ examples/openmdao/plot_results.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import openmdao.api as om
+import numpy as np
+
+"""
+This file can be used to plot results from the cases.sql file produced from
+optimization or parameter sweep run files. The cases.sql file must be in the same folder
+to plot results. All plots will be saved in the current directory and not displayed.
+
+This file is intended for a single design variable case; it will plot the first design variable
+against all openmdao outputs specified in the openmdao_wrapper.py as well as the objective function
+specified in the optimization runs. 
+
+Note: if generation profiles are specified as outputs in the wrapper.py this file will not plot them
+as they are times series arrays.
+"""
+
+cr = om.CaseReader("cases.sql")
+cases = cr.list_cases('driver')
+
+results = {}
+for case in cases:
+    outputs = cr.get_case(case).outputs
+    for key in outputs:
+
+        if key not in results.keys():
+            results[key] = []
+
+        results[key].append(outputs[key])
+
+for key in results:
+    results[key] = np.array(results[key]) 
+
+# Plot DV vs various objectives, intended for one design variable and 1+ objectives
+des_var = list(cr.get_case(case).get_design_vars().keys())[0]
+no_plot_outputs = ['pv_generation_profile', 
+                   'wind_generation_profile', 
+                   'hybrid_generation_profile', 
+                   'pv_resource_gh', 
+                   'wind_resource_speed', 
+                   'wind_resource_temp', 
+                   'wind_resource_press', 
+                   'wind_resource_dir',
+                   ]
+
+## determine max/min point found in parameter sweep or optimization run
+# hybrid_npv
+print('max hybrid_npv = ', np.max(results['hybrid_npv']))
+max_index = np.where(results['hybrid_npv'] == np.max(results['hybrid_npv']))
+print('wind_fraction for max hybrid_npv = ', results[des_var][max_index][0])
+
+#hybrid_lcoe_real
+print('min hybrid_lceo_real = ', np.min(results['hybrid_lcoe_real']))
+min_index = np.where(results['hybrid_lcoe_real'] == np.min(results['hybrid_lcoe_real']))
+print('wind_fraction for min hybrid_lcoe_real = ', results[des_var][min_index][0])
+
+#hybrid_annual_energy
+print('max hybrid_annual_energy = ', np.max(results['hybrid_annual_energy']))
+max_index = np.where(results['hybrid_annual_energy'] == np.max(results['hybrid_annual_energy']))
+print('wind_fraction for max hybrid_annual_energy = ', results[des_var][max_index][0])
+
+for key in results:
+    if key != des_var and key not in no_plot_outputs :
+        plt.scatter(results[des_var],results[key])
+        plt.xlabel('{}'.format(des_var))
+        plt.ylabel('{}'.format(key))
+        fname = '{}_vs_{}.png'.format(des_var,key)
+        plt.savefig(fname)
+        plt.clf()
+    # plt.show()
+
--- examples/openmdao/run_openmdao_optimization.py
+++ examples/openmdao/run_openmdao_optimization.py
@@ -1,6 +1,7 @@
 import openmdao.api as om
 import json
 from pathlib import Path
+import time
 
 from hybrid.openmdao_wrapper import HybridSystem
 from hybrid.sites import SiteInfo, flatirons_site
@@ -8,7 +9,9 @@ from hybrid.hybrid_simulation import HybridSimulation
 from hybrid.log import hybrid_logger as logger
 from hybrid.keys import set_nrel_key_dot_env
 
-        
+start_time = time.time()
+
+
 resource_files_dir = Path(__file__).parent.absolute().parent.absolute().parent.absolute() / 'resource_files'
 
 # Set API key
@@ -17,12 +20,13 @@ set_nrel_key_dot_env()
 if __name__ == "__main__":
     ### build the model
     prob = om.Problem()
-    prob.model.add_subsystem('hybrid_system', HybridSystem(location=flatirons_site, battery=False, grid=True, sim_duration_years=25, hybrid_rated_power=100), promotes=['*'])
+    prob.model.add_subsystem('hybrid_system', HybridSystem(location=flatirons_site, battery=False, grid=True, sim_duration_years=25, hybrid_rated_power=250), promotes_inputs=['wind_fraction'], promotes_outputs=['hybrid_npv'])
 
     ### setup the optimization
     prob.driver = om.ScipyOptimizeDriver()
-    prob.driver.options['optimizer'] = 'SLSQP'
-    prob.driver.options['tol'] = 1e-4
+    prob.driver.options['optimizer'] = 'trust-constr'
+    prob.driver.options['tol'] = 1e-3
+    prob.driver.options['disp'] = True
     # prob.driver = om.DOEDriver(om.FullFactorialGenerator(levels=76))
     prob.driver.options['debug_print'] = ["desvars", "objs"]
     prob.driver.add_recorder(om.SqliteRecorder("cases.sql"))
@@ -31,9 +35,10 @@ if __name__ == "__main__":
     ### setup design variables
     # Solar DVs
     # prob.model.add_design_var('solar_size_mw', lower=0.001, upper=15.)
+    # prob.model.add_design_var('solar_fraction', lower=0.001, upper=0.999)
 
     # Wind DVs
-    prob.model.add_design_var('wind_fraction', lower=0.001, upper=0.999)
+    prob.model.add_design_var('wind_fraction', lower=0., upper=1.)
     # prob.model.add_design_var('wind_size_mw', lower=0.001, upper=15.)
     # prob.model.add_design_var('turbine_rating_kw', lower=10, upper=14000)
 
@@ -44,18 +49,24 @@ if __name__ == "__main__":
     # Grid DVs
     # prob.model.add_design_var('interconnection_size_mw', lower=0., upper=5.)
 
+    # COBYLA Constraints:
+    prob.model.add_constraint('wind_fraction', lower=0., upper=1.)
+
     ## setup objective function
-    # prob.model.add_objective('hybrid_npv', ref=1.)
-    prob.model.add_objective('hybrid_lcoe_real', ref=-1.)
-    # prob.model.add_objective('hybrid_irr', ref=1.)
+    prob.model.add_objective('hybrid_npv', ref=-1.e9)
+    # prob.model.add_objective('hybrid_lcoe_real', ref=1.)
+    # prob.model.add_objective('hybrid_annual_energy', ref=-1.e9)
+ 
      
-    prob.model.approx_totals()
-    
+    prob.model.approx_totals(method='fd', step=1e-3, form='forward')
+
     prob.setup()
     prob.run_driver()
-    
-    prob.model.list_outputs()
-    
 
-    
-    
+    print('design variable =', prob['wind_fraction'])
+    print('objective =', prob['hybrid_npv'])
+
+    prob.model.list_outputs()
+
+    end_time = time.time()
+    print('run time = ', (end_time - start_time))
