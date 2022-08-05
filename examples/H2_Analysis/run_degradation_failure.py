1cf01c5eb55a6b50007289d489f7df2bbe44162c
kbrunik <kbrunik@gmail.com>
2022-08-05 15:10:47 -0700

    added battery degradation and failure to run file.

--- examples/H2_Analysis/run_degradation_failure.py
+++ examples/H2_Analysis/run_degradation_failure.py
@@ -16,12 +16,12 @@ examples_dir = Path(__file__).resolve().parents[1]
 set_nrel_key_dot_env()
 
 # Set wind, solar, and interconnection capacities (in MW)
-solar_size_mw = 50
-wind_size_mw = 50
-interconnection_size_mw = 50        #Required by HybridSimulation() not currently being used for calculations.
-battery_capacity_mw = 20
+solar_size_mw = 560
+wind_size_mw = 560
+interconnection_size_mw = 1000        #Required by HybridSimulation() not currently being used for calculations.
+battery_capacity_mw = 100
 battery_capacity_mwh = battery_capacity_mw * 4 
-electrolyzer_capacity_mw = 40
+electrolyzer_capacity_mw = 100
 useful_life = 30
 load = [electrolyzer_capacity_mw*1000] * useful_life * 8760
 
@@ -75,18 +75,32 @@ degraded_generation['wind'] = wind_deg
 # Use degraded generation profiles
 hybrid_failure = Failure(technologies,False, useful_life,degraded_generation,load, False)
 
-# # Add failures to generation technology
+# Add failures to generation technology (pv and wind)
 hybrid_failure.simulate_generation_failure()
 
-# hybrid_degradation.hybrid_degraded_generation = [0]*8760*useful_life
-# hybrid_degradation.pv_degraded_generation = [0]*8760*useful_life
-# hybrid_degradation.wind_degraded_generation = [0]*8760*useful_life
-# hybrid_degradation.simulate_battery_degradation()
+# Feeds generation profile that has degradation and failure into battery simulation
+pv_deg_fail = [x for x in hybrid_failure.pv_failed_generation]
+wind_deg_fail = [x for x in hybrid_failure.wind_failed_generation]
+hybrid_degradation.hybrid_degraded_generation = np.add(pv_deg_fail, wind_deg_fail)
+
+# Simulates battery degradation
+hybrid_degradation.simulate_battery_degradation()
+
+battery_deg = [x for x in hybrid_degradation.battery_used]
+
+# Simulates battery failure
+hybrid_failure.battery_used = battery_deg
+
+hybrid_failure.simulate_battery_failure(input_battery_use=True)
+battery_deg_fail = [x for x in hybrid_failure.battery_used]
+
 # hybrid_degradation.simulate_electrolyzer_degradation()
 # print("Number of battery repairs: ", hybrid_degradation.battery_repair)
 # print("Number of electrolyzer repairs: ", hybrid_degradation.electrolyzer_repair)
 print("Number of pv repairs: ", hybrid_failure.pv_repair)
 print("Number of wind repairs: ", hybrid_failure.wind_repair)
+print("Number of battery repairs: ", hybrid_degradation.battery_repair)
+print('Number of battery repairs: ', hybrid_failure.battery_repair_failure)
 
 print("Non-degraded lifetime pv power generation: ", np.sum(hybrid_plant.pv.generation_profile)/1000, "[MW]")
 print("Degraded lifetime pv power generation: ", np.sum(hybrid_degradation.pv_degraded_generation)/1000, "[MW]")
@@ -96,7 +110,8 @@ print("Non-degraded lifetime wind power generation: ", np.sum(hybrid_plant.wind.
 print("Degraded lifetime wind power generation: ", np.sum(hybrid_degradation.wind_degraded_generation)/1000, "[MW]")
 print('Failed wind power generation: ', np.sum(hybrid_failure.wind_failed_generation)/1000, "[MW]")
 
-# print("Battery used over lifetime: ", np.sum(hybrid_degradation.battery_used)/1000, "[MW]")
+print("Battery used over lifetime (Degradation): ", np.sum(hybrid_degradation.battery_used)/1000, "[MW]")
+print("Battery used over lifetime (Degradation + Failure): ", np.sum(hybrid_failure.battery_used)/1000, "[MW]")
 # print("Life-time Hydrogen production: ", np.sum(hybrid_degradation.hydrogen_hourly_production), "[kg]")
 
 plot_degradation = False
