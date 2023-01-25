122d61bc4e62f5085c286172d2c357ac32b1bea4
Cory Frontin <cory.frontin@nrel.gov>
2023-01-25 09:45:18 -0500

    pythonic tankinator added, tested against tankinator up to bug in tankinator.

--- hopp/hydrogen/h2_storage/pressure_vessel/tankinator.py
+++ hopp/hydrogen/h2_storage/pressure_vessel/tankinator.py
@@ -22,7 +22,7 @@ class MetalMaterial(object):
     a class for the material properties for metals used in analysis
 
     :param metal_type: type of metal to use, must be defined in
-            metalmaterial_properties.json
+            material_properties.json
     :type metal_type: str, must be in list of known materials
     :param approx_method: method to approximate inexact table lookups
     :type approx_method: str, must be in list of known methods
@@ -38,11 +38,11 @@ class MetalMaterial(object):
                                       + "(%s) is not implemented." % approx_method)
         
         with open(os.path.join(os.path.split(__file__)[0],
-                  "metalmaterial_properties.json"), "r") as mmprop_file:
+                  "material_properties.json"), "r") as mmprop_file:
             mmprop= json.load(mmprop_file)
         if metal_type not in mmprop.keys():
             raise NotImplementedError("the requested metal/material (%s) has not" % metal_type \
-                                      + "been implemented in metalmaterial_properties.json.\n" \
+                                      + "been implemented in material_properties.json.\n" \
                                       + "available types:", mmprop.keys())
         self.mmprop= mmprop[metal_type] # select the relevant material property set
 
@@ -100,19 +100,17 @@ class Tank(object):
     """
     def __init__(self,
                  tank_type : int,
-                 material : str,
                  yield_factor : float= 3./2.,
-                 ultimate_factor : float= 2.25,
-                 shear_approx = "interp"):
+                 ultimate_factor : float= 2.25):
+
         # unpack the key variables
         if not tank_type in [1, 3, 4]:
             raise NotImplementedError("tank_type %d has not been implemented yet.\n" % tank_type)
         self.tank_type= tank_type
 
-        if tank_type == 1:
-            self.material= MetalMaterial(material, approx_method= shear_approx)
-        else:
-            raise NotImplementedError("haven't done other classes yet. -CVF")            
+        # if not (tank_type == 1):
+        #     raise NotImplementedError("haven't done other classes yet. -CVF")
+        # self.liner= None
 
         # store fixed attributes
         self.yield_factor= yield_factor
@@ -121,7 +119,6 @@ class Tank(object):
         # to start up: undefined geometry values
         self.length_inner= None # inner total length of tank (m)
         self.radius_inner= None # inner radius of tank (m)
-        self.thickness= None # thickness of tank
 
         # operating conditions
         self.operating_temperature= None
@@ -141,8 +138,6 @@ class Tank(object):
         return self.operating_temperature
     def get_operating_pressure(self):
         return self.operating_pressure
-    def get_thickness(self):
-        return self.thickness
 
     # set functions: specify two of (length, radius, volume)
     def set_length_radius(self, length_in, radius_in):
@@ -182,6 +177,40 @@ class Tank(object):
     def set_operating_pressure(self, pressure_in):
         self.operating_pressure = pressure_in
 
+    # useful static methods
+    def compute_hemicylinder_volume(R : float, L : float) -> float:
+        assert (L >= 2*R) # cylindrical tank with hemispherical ends
+        return np.pi*R**2*(L - 2.*R/3.)
+    def compute_hemicylinder_outer_length(L : float, t : float) -> float:
+        return L + 2*t
+    def compute_hemicylinder_outer_radius(R : float, t : float) -> float:
+        return R + t
+    def check_thinwall(Rinner : float, t : float, thinwallratio_crit= 10) -> bool:
+        return (Rinner/t >= thinwallratio_crit)
+
+class TypeITank(Tank):
+    """
+    a class I tank: metal shell tank
+    """
+   
+    def __init__(self,
+                 material : str,
+                 yield_factor : float= 3./2.,
+                 ultimate_factor : float= 2.25,
+                 shear_approx = "interp"):
+
+        super().__init__(1, yield_factor, ultimate_factor)
+        
+        self.material= MetalMaterial(material, approx_method= shear_approx)
+
+        # initial geometry values undefined
+        self.thickness= None # thickness of tank
+
+    # return functions for symmetry
+    def get_thickness(self):
+        return self.thickness
+    
+    # get the outer dimensions
     def get_length_outer(self):
         """ returns the outer length of the pressure vessel in cm """
         if None in [self.length_inner, self.thickness]:
@@ -209,13 +238,11 @@ class Tank(object):
         if None in [volume_inner, volume_outer]: return None
         assert volume_outer >= volume_inner
         return (volume_outer - volume_inner)
-
     def get_mass_metal(self):
         """ returns the mass of the pressure vessel in kg """
         volume_metal= self.get_volume_metal()
         if volume_metal is None: return None
         return self.material.density*volume_metal
-
     def get_cost_metal(self):
         """
         returns the cost of the metal in the pressure vessel in dollars
@@ -270,7 +297,6 @@ class Tank(object):
         thickness_yield= pressure*self.radius_inner/Sy*self.yield_factor
 
         return thickness_yield
-
     def get_ultimate_thickness(self,
                                pressure : float = None,
                                temperature : float = None):
@@ -331,7 +357,6 @@ class Tank(object):
         thickness= max(t_y, t_u)
 
         return thickness
-    
     def set_thickness_thinwall(self,
                                pressure : float = None,
                                temperature : float = None):
@@ -401,8 +426,6 @@ class Tank(object):
                                     max_iter= max_cycle_iter,
                                     WTAF_tol= adj_fac_tol)
         return thickness_cycle, iter_cycle
-
-
     def set_thickness_vonmises(self,
                                pressure : float = None,
                                temperature : float = None,
@@ -427,27 +450,246 @@ class Tank(object):
                                                      max_cycle_iter, adj_fac_tol)
         self.thickness= thickness
 
-    # useful static methods
-    def compute_hemicylinder_volume(R : float, L : float) -> float:
-        assert (L >= 2*R) # cylindrical tank with hemispherical ends
-        return np.pi*R**2*(L - 2.*R/3.)
-    def compute_hemicylinder_outer_length(L : float, t : float) -> float:
-        return L + 2*t
-    def compute_hemicylinder_outer_radius(R : float, t : float) -> float:
-        return R + t
-    def check_thinwall(Rinner : float, t : float, thinwallratio_crit= 10) -> bool:
-        return (Rinner/t >= thinwallratio_crit)
+    
 
+class LinedTank(Tank):
+    """
+    a lined tank for Type III or Type III: aluminum-lined carbon fiber-jacketed tank
+    """
 
+    def __init__(self,
+                 tanktype : int,
+                 load_bearing_liner,
+                 liner_design_load_factor= 0.21,
+                 liner_thickness_min = 0.3,
+                 yield_factor : float= 3./2.,
+                 ultimate_factor : float= 2.25):
+        super().__init__(tanktype, yield_factor, ultimate_factor)
 
+        if tanktype not in [3, 4]:
+            raise NotImplementedError("unknown tank type.")
 
+        self.load_bearing_liner= load_bearing_liner
+        self.liner_design_load_factor= liner_design_load_factor
 
+        with open(os.path.join(os.path.split(__file__)[0],
+                  "material_properties.json"), "r") as mmprop_file:
+            mmprop= json.load(mmprop_file)
 
-# class TypeITank(Tank):
-#     """
-#     a class I tank: metal shell tank
-#     """
-#    
-#     def __init__(self,
-#                  material : str):
-#         super().__init__(1, material)
+        # liner characteristics
+        if tanktype == 3:
+            self.shear_ultimate_liner= mmprop["liner_aluminum"]["shear_ultimate_bar"]
+            self.density_liner= mmprop["liner_aluminum"]["density_kgccm"]
+            self.costrate_liner= mmprop["liner_aluminum"]["costrate_$kg"]
+        else:
+            self.shear_ultimate_liner= None
+            self.density_liner= mmprop["HDPE"]["density_kgccm"]
+            self.costrate_liner= mmprop["HDPE"]["costrate_$kg"]
+        self.thickness_liner_min= liner_thickness_min
+        
+        # jacket characteristics
+        self.shear_ultimate_jacket= mmprop["carbon_fiber"]["shear_ultimate_bar"]
+        self.density_jacket= mmprop["carbon_fiber"]["density_kgccm"]
+        self.costrate_jacket= mmprop["carbon_fiber"]["costrate_$kg"]
+        self.fiber_translation_efficiency_jacket= mmprop["carbon_fiber"]["fiber_translation_efficiency"]
+        self.fiber_layer_thickness= mmprop["carbon_fiber"]["layer_thickness_cm"]
+        self.fiber_layer_min= mmprop["carbon_fiber"]["min_layer"]
+
+        # thicknesses (to be computed)
+        self.thickness_liner= None
+        self.thickness_jacket= None
+        self.thickness_ideal_jacket= None
+        self.Nlayer_jacket= None
+
+    def get_thicknesses_thinwall(self, pressure : float = None):
+        """ ??? """
+
+        if (pressure is None) and (self.operating_pressure is None):
+            raise LookupError("you must specify an operating pressure.")
+        elif pressure is None:
+            pressure= self.operating_pressure
+
+        # compute the liner thickness
+        pressure_burst_target= self.ultimate_factor*pressure
+        if self.tank_type != 4:
+            pressure_liner_target= pressure_burst_target*self.liner_design_load_factor
+            thickness_burst= pressure_liner_target*self.radius_inner/self.shear_ultimate_liner
+            thickness_liner= max(thickness_burst, self.thickness_liner_min)
+        else:
+            thickness_liner= self.thickness_liner_min
+
+        # compute ideal jacket thickness
+        radius_liner= Tank.compute_hemicylinder_outer_radius(self.radius_inner, thickness_liner)
+        thickness_jacket_ideal= pressure_burst_target*radius_liner \
+                /self.shear_ultimate_jacket/self.fiber_translation_efficiency_jacket
+        if self.load_bearing_liner:
+            # subdivide pressure if liner is load-bearing
+            pressure_liner= thickness_liner*self.shear_ultimate_liner/self.radius_inner
+            pressure_jacket= pressure_burst_target - pressure_liner
+            assert pressure_jacket >= 0
+            thickness_jacket_ideal= pressure_jacket*radius_liner/self.shear_ultimate_jacket \
+                    /self.fiber_translation_efficiency_jacket
+
+        # compute number of layers, real thickness
+        Nlayer_jacket= max(self.fiber_layer_min,
+                           int(np.ceil(thickness_jacket_ideal/self.fiber_layer_thickness)))
+        thickness_jacket_real= Nlayer_jacket*self.fiber_layer_thickness
+
+        return thickness_liner, thickness_jacket_ideal, Nlayer_jacket, thickness_jacket_real
+
+    def set_thicknesses_thinwall(self, pressure : float = None):
+        """ ??? """
+
+        if (pressure is None) and (self.operating_pressure is None):
+            raise LookupError("you must specify an operating pressure.")
+        elif pressure is None:
+            pressure= self.operating_pressure
+
+        # pass the returns of the previous function
+        self.thickness_liner, self.thickness_ideal_jacket, \
+                self.Nlayer_jacket, self.thickness_jacket= \
+                self.get_thicknesses_thinwall(pressure)
+
+    def get_safetyfactor_real_jacket(self):
+        """
+        figure out the integer layer safety factor
+        """
+
+        if None in [self.thickness_jacket, self.thickness_ideal_jacket]: return None
+
+        sf_real= self.ultimate_factor*self.thickness_jacket/self.thickness_jacket_ideal
+        
+        return sf_real
+
+    # get the liner dimensions
+    def get_length_liner(self):
+        """ returns the outer length of the pressure vessel in cm """
+        if None in [self.length_inner, self.thickness_liner]:
+            return None
+        return Tank.compute_hemicylinder_outer_length(self.length_inner, self.thickness_liner)
+    def get_radius_liner(self):
+        """ returns the outer radius of the pressure vessel in cm """
+        if None in [self.radius_inner, self.thickness_liner]:
+            return None
+        return Tank.compute_hemicylinder_outer_radius(self.radius_inner, self.thickness_liner)
+    def get_volume_outer_liner(self):
+        """
+        returns the outer volume of the pressure vessel in ccm
+        """
+        if None in [self.length_inner, self.radius_inner, self.thickness_liner]:
+            return None
+        return Tank.compute_hemicylinder_volume(self.get_radius_liner(), self.get_length_liner())
+    def get_volume_liner(self):
+        """
+        returns the (unsealed) displacement volume of the liner in ccm
+        """
+        volume_inner= self.get_volume_inner()
+        volume_outer_liner= self.get_volume_outer_liner()
+        if None in [volume_inner, volume_outer_liner]: return None
+        assert volume_outer_liner >= volume_inner
+        return (volume_outer_liner - volume_inner)
+    def get_mass_liner(self):
+        """
+        returns the mass of the liner in kg
+        """
+        volume_liner= self.get_volume_liner()
+        if volume_liner is None: return None
+        return self.density_liner*volume_liner
+    def get_cost_liner(self):
+        """
+        returns the cost of the liner material in $
+        """
+        mass_liner= self.get_mass_liner()
+        if mass_liner is None: return None
+        return self.costrate_liner*mass_liner
+
+    # get the outer dimensions
+    def get_length_outer(self):
+        """ returns the outer length of the pressure vessel in cm """
+        if None in [self.length_inner, self.thickness_liner, self.thickness_jacket]:
+            return None
+        return Tank.compute_hemicylinder_outer_length(self.length_inner,
+                self.thickness_liner + self.thickness_jacket)
+    def get_radius_outer(self):
+        """ returns the outer radius of the pressure vessel in cm """
+        if None in [self.radius_inner, self.thickness_liner, self.thickness_jacket]:
+            return None
+        return Tank.compute_hemicylinder_outer_radius(self.radius_inner,
+                self.thickness_liner + self.thickness_jacket)
+    def get_volume_outer(self):
+        """
+        returns the outer volume of the pressure vessel in ccm
+        """
+        if None in [self.length_inner, self.radius_inner, self.thickness_liner, self.thickness_jacket]:
+            return None
+        return Tank.compute_hemicylinder_volume(self.get_radius_outer(), self.get_length_outer())
+    def get_volume_jacket(self):
+        """
+        returns the (unsealed) displacement volume of the carbon fiber jacket in ccm
+        """
+        volume_outer= self.get_volume_outer()
+        volume_outer_liner= self.get_volume_outer_liner()
+        if None in [volume_outer, volume_outer_liner]: return None
+        assert volume_outer >= volume_outer_liner
+        return (volume_outer - volume_outer_liner)
+    def get_mass_jacket(self):
+        """
+        returns the mass of the carbon fiber jacket in kg
+        """
+        volume_jacket= self.get_volume_jacket()
+        if volume_jacket is None: return None
+        return self.density_jacket*volume_jacket
+    def get_cost_jacket(self):
+        """
+        returns the cost of the jacket material in $
+        """
+        mass_jacket= self.get_mass_jacket()
+        if mass_jacket is None: return None
+        return self.costrate_jacket*mass_jacket
+    
+    def get_mass_tank(self):
+        """
+        returns the mass of the empty tank in kg
+        """
+        mass_liner= self.get_mass_liner()
+        mass_jacket= self.get_mass_jacket()
+        if None in [mass_liner, mass_jacket]: return None
+        return mass_liner + mass_jacket
+    def get_cost_tank(self):
+        """
+        returns the material cost of the tank in $
+        """
+        cost_liner= self.get_cost_liner()
+        cost_jacket= self.get_cost_jacket()
+        if None in [cost_liner, cost_jacket]: return None
+        return cost_liner + cost_jacket
+    def get_gravimetric_tank_efficiency(self):
+        """
+        returns the gravimetric tank efficiency:
+        $$ \frac{m_{tank}}{V_{inner}} $$
+        in L/kg
+        """
+        mass_tank= self.get_mass_tank()
+        volume_inner= self.get_volume_inner()
+        return (volume_inner/1e3)/mass_tank
+
+class TypeIIITank(LinedTank):
+    def __init__(self,
+                 conservative= False,
+                 liner_design_load_factor= 0.21,
+                 liner_thickness_min= 0.3,
+                 yield_factor: float = 3 / 2,
+                 ultimate_factor: float = 2.25):
+                load_bearing_liner= not conservative # use load bearing liner iff not conservative
+                super().__init__(3, load_bearing_liner, liner_design_load_factor,
+                         liner_thickness_min, yield_factor, ultimate_factor)
+
+class TypeIVTank(LinedTank):
+    def __init__(self,
+                 liner_design_load_factor= 0.21,
+                 liner_thickness= 0.4,
+                 yield_factor: float = 3/2,
+                 ultimate_factor: float = 2.25):
+        super().__init__(4, False, liner_design_load_factor,
+                         liner_thickness, yield_factor, ultimate_factor)
+
--- tests/hopp/test_hydrogen/test_tankinator.py
+++ tests/hopp/test_hydrogen/test_tankinator.py
@@ -1,5 +1,5 @@
 
-from hopp.hydrogen.h2_storage.pressure_vessel.tankinator import Tank
+from hopp.hydrogen.h2_storage.pressure_vessel.tankinator import Tank, TypeITank, TypeIIITank, TypeIVTank
 import hopp.hydrogen.h2_storage.pressure_vessel.von_mises as von_mises
 import pytest
 import numpy as np
@@ -8,19 +8,17 @@ import numpy as np
 class TestTankinator():
 
     def test_tank_type(self):
-        tank1= Tank(1, '6061_T6_Aluminum')
-        # tank3= Tank(3, '316SS')
-        # tank4= Tank(4, 'steel')
+        tank1= TypeITank('6061_T6_Aluminum')
+        tank3= TypeIIITank()
+        tank4= TypeIVTank()
 
         assert tank1.tank_type == 1
-        # assert tank3.tank_type == 3
-        # assert tank4.tank_type == 4
+        assert tank3.tank_type == 3
+        assert tank4.tank_type == 4
 
-    def test_tank_type_exc(self):
-        with pytest.raises(TypeError):
-            Tank(2)
-        with pytest.raises(TypeError):
-            Tank("x")
+    # def test_tank_type_exc(self):
+    #     with pytest.raises(TypeError):
+    #         TypeITank(2)
 
     def test_hemicylindrical_static(self):
         """ a random sphere, and a random cylinder """
@@ -45,7 +43,7 @@ class TestTankinator():
         assert Tank.compute_hemicylinder_outer_radius(radius, thickness) == pytest.approx(radius + thickness)
         assert Tank.compute_hemicylinder_volume(radius, length) == pytest.approx(volume_exact)
 
-    def test_tank_geometric(self):
+    def test_tankI_geometric(self):
         """ make sure geometric calls work correctly """
 
         # L = 2R; should reduce to a sphere!
@@ -55,7 +53,7 @@ class TestTankinator():
         volume_exact= 4./3.*np.pi*radius**3
 
         # create tank, set dimensions, check dimensioning
-        tank= Tank(1, "6061_T6_Aluminum")
+        tank= TypeITank("6061_T6_Aluminum")
         tank.set_length_radius(length, radius)
         tank.thickness = thickness # manual override
         tank.volume_inner = Tank.compute_hemicylinder_volume(tank.get_radius_inner(),
@@ -79,7 +77,7 @@ class TestTankinator():
 
         assert tank.get_gravimetric_tank_efficiency() == pytest.approx((volume_exact/1e3)/mass_exact)
     
-    def test_tank_set_functions(self):
+    def test_tankI_set_functions(self):
         """ make sure that the inverse geometry spec works """
 
         radius= 4.3
@@ -98,7 +96,7 @@ class TestTankinator():
         tank.set_radius_volume(radius, volume_exact)
         assert tank.get_length_inner() == pytest.approx(length)
 
-    def test_tankinator_comp(self):
+    def test_tankinator_typeI_comp(self):
         """ compare to the tankinator case """
 
         T_op= -50 # degC
@@ -143,7 +141,7 @@ class TestTankinator():
         cost_f_ref= 9041.80
 
         # set up w/ lookup shear approximation
-        tank= Tank(1, "6061_T6_Aluminum", shear_approx= "lookup")
+        tank= TypeITank("6061_T6_Aluminum", shear_approx= "lookup")
         tank.set_operating_temperature(T_op)
         tank.set_operating_pressure(p_op)
         tank.set_length_volume(Ltank, Vtank)
@@ -202,6 +200,136 @@ class TestTankinator():
         assert tank.get_thickness() == pytest.approx(thickness_f_ref)
         assert tank.get_mass_metal() == pytest.approx(mass_f_ref, abs= 0.1)
         assert tank.get_cost_metal() == pytest.approx(cost_f_ref, abs= 0.01)    
+
+    def test_tankinator_typeIII_comp(self):
+        """ compare to the tankinator case """
+
+        T_op= 20. # degC
+        p_op= 250. # bar
+        Rtank= 16.0
+        Ltank= 1219. # cm
+        Vtank= 971799. # ccm
+
+        # reference values from the excel sheet default values, best estimate
+        R_ref= 16.0
+        thickness_liner_ref= 0.61
+        thickness_ideal_jacket_ref= 0.602759006
+        Nlayer_jacket_ref= 7
+        thickness_jacket_ref= 0.64008
+        length_liner_ref= 1220.218176
+        radius_liner_ref= 16.60908798
+        V_outer_liner_ref= 1047.900361*1000
+        V_liner_ref= 76101.03372
+        m_liner_ref= 202.66
+        cost_liner_ref= 901.82
+        length_outer_ref= 1221.498336
+        radius_outer_ref= 17.24916798
+        V_outer_ref= 1131.022248*1000
+        V_jacket_ref= 83121.88687
+        m_jacket_ref= 133.91
+        cost_jacket_ref= 4104.32
+        m_tank_ref= 336.57
+        cost_tank_ref= 5006.15
+        gravimetric_tank_efficiency_ref= 2.89
+
+        # set up w/ lookup shear approximation
+        tank= TypeIIITank()
+        tank.set_operating_temperature(T_op)
+        tank.set_operating_pressure(p_op)
+        tank.set_length_radius(Ltank, Rtank)
+
+        tank.set_thicknesses_thinwall()
+
+        # check against reference values
+        assert tank.get_radius_inner() == pytest.approx(R_ref)
+        assert tank.get_volume_inner() == pytest.approx(Vtank)
+        assert tank.thickness_liner == pytest.approx(thickness_liner_ref, abs= 0.01)
+        assert tank.thickness_ideal_jacket == pytest.approx(thickness_ideal_jacket_ref)
+        assert tank.Nlayer_jacket == pytest.approx(Nlayer_jacket_ref)
+        assert tank.thickness_jacket == pytest.approx(thickness_jacket_ref)
+
+        assert tank.get_length_liner() == pytest.approx(length_liner_ref)
+        assert tank.get_radius_liner() == pytest.approx(radius_liner_ref)
+        assert tank.get_volume_outer_liner() == pytest.approx(V_outer_liner_ref)
+        assert tank.get_volume_liner() == pytest.approx(V_liner_ref)
+
+        assert tank.get_length_outer() == pytest.approx(length_outer_ref)
+        assert tank.get_radius_outer() == pytest.approx(radius_outer_ref)
+        assert tank.get_volume_outer() == pytest.approx(V_outer_ref)
+        assert tank.get_volume_jacket() == pytest.approx(V_jacket_ref)
+
+        assert tank.get_mass_liner() == pytest.approx(m_liner_ref, abs= 0.01)
+        assert tank.get_mass_jacket() == pytest.approx(m_jacket_ref, abs= 0.01)
+        assert tank.get_cost_liner() == pytest.approx(cost_liner_ref, abs= 0.01)
+        assert tank.get_cost_jacket() == pytest.approx(cost_jacket_ref, abs= 0.01)
+        assert tank.get_mass_tank() == pytest.approx(m_tank_ref, abs= 0.01)
+        assert tank.get_cost_tank() == pytest.approx(cost_tank_ref, abs= 0.01)
+        assert tank.get_gravimetric_tank_efficiency() == pytest.approx(gravimetric_tank_efficiency_ref, abs= 0.01)
+
+    def test_tankinator_typeIV_comp(self):
+        """ compare to the tankinator case """
+
+        T_op= 20. # degC
+        p_op= 350. # bar
+        Rtank= 50.
+        Ltank= 1000. # cm
+        Vtank= 7592182. # ccm
+
+        # reference values from the excel sheet default values, best estimate
+        R_ref= 50.0
+        thickness_liner_ref= 0.4
+        thickness_ideal_jacket_ref= 3.241375931
+        Nlayer_jacket_ref= 36
+        thickness_jacket_ref= 3.29184
+        # length_liner_ref= 1220.218176
+        radius_liner_ref= 50.4
+        # V_outer_liner_ref= 1047.900361*1000
+        # V_liner_ref= 76101.03372
+        # m_liner_ref= 202.66
+        # cost_liner_ref= 901.82
+        # length_outer_ref= 1221.498336
+        # radius_outer_ref= 17.24916798
+        # V_outer_ref= 1131.022248*1000
+        # V_jacket_ref= 83121.88687
+        # m_jacket_ref= 133.91
+        # cost_jacket_ref= 4104.32
+        # m_tank_ref= 336.57
+        # cost_tank_ref= 5006.15
+        # gravimetric_tank_efficiency_ref= 2.89
+
+        # set up w/ lookup shear approximation
+        tank= TypeIVTank()
+        tank.set_operating_temperature(T_op)
+        tank.set_operating_pressure(p_op)
+        tank.set_length_radius(Ltank, Rtank)
+
+        tank.set_thicknesses_thinwall()
+
+        # check against reference values
+        assert tank.get_radius_inner() == pytest.approx(R_ref)
+        assert tank.get_volume_inner() == pytest.approx(Vtank)
+        assert tank.thickness_liner == pytest.approx(thickness_liner_ref, abs= 0.01)
+        assert tank.thickness_ideal_jacket == pytest.approx(thickness_ideal_jacket_ref)
+        assert tank.Nlayer_jacket == pytest.approx(Nlayer_jacket_ref)
+        assert tank.thickness_jacket == pytest.approx(thickness_jacket_ref)
+
+        # assert tank.get_length_liner() == pytest.approx(length_liner_ref)
+        assert tank.get_radius_liner() == pytest.approx(radius_liner_ref)
+        # assert tank.get_volume_outer_liner() == pytest.approx(V_outer_liner_ref)
+        # assert tank.get_volume_liner() == pytest.approx(V_liner_ref)
+
+        # assert tank.get_length_outer() == pytest.approx(length_outer_ref)
+        # assert tank.get_radius_outer() == pytest.approx(radius_outer_ref)
+        # assert tank.get_volume_outer() == pytest.approx(V_outer_ref)
+        # assert tank.get_volume_jacket() == pytest.approx(V_jacket_ref)
+
+        # assert tank.get_mass_liner() == pytest.approx(m_liner_ref, abs= 0.01)
+        # assert tank.get_mass_jacket() == pytest.approx(m_jacket_ref, abs= 0.01)
+        # assert tank.get_cost_liner() == pytest.approx(cost_liner_ref, abs= 0.01)
+        # assert tank.get_cost_jacket() == pytest.approx(cost_jacket_ref, abs= 0.01)
+        # assert tank.get_mass_tank() == pytest.approx(m_tank_ref, abs= 0.01)
+        # assert tank.get_cost_tank() == pytest.approx(cost_tank_ref, abs= 0.01)
+        # assert tank.get_gravimetric_tank_efficiency() == pytest.approx(gravimetric_tank_efficiency_ref, abs= 0.01)
 
 if __name__ == "__main__":
     test_set = test_tankinator()
