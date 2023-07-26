6b4cb130c95a0f1bf6042f5296bbc425639369a3
Kodanda Pani <nkodanda@nrel.gov>
2023-07-26 13:42:15 -0600

    Update hopp_for_h2_floris.py

--- examples/H2_Analysis/hopp_for_h2_floris.py
+++ examples/H2_Analysis/hopp_for_h2_floris.py
@@ -5,11 +5,15 @@ import json
 from tools.analysis import create_cost_calculator
 import pandas as pd
 
-def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw, storage_size_mw, storage_size_mwh, storage_hours,
-                wind_cost_kw, solar_cost_kw, storage_cost_kw, storage_cost_kwh,
-                kw_continuous, load,
-                custom_powercurve,
-                interconnection_size_mw, grid_connected_hopp=True, wind_om_cost_kw = 42, turbine_parent_path=None, ppa_price=0.0):
+
+def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw, wave_size_mw, storage_size_mw, storage_size_mwh, storage_hours,
+                       wind_cost_kw, solar_cost_kw, storage_cost_kw, storage_cost_kwh,
+                       kw_continuous, load,
+                       custom_powercurve,
+                       interconnection_size_mw,
+                       grid_connected_hopp=True, wind_om_cost_kw=42, 
+                       solar_om_cost_kw=0,
+                       turbine_parent_path=None, ppa_price=0.0):
     '''
     Runs HOPP for H2 analysis purposes
     :param site: :class:`hybrid.sites.site_info.SiteInfo`,
@@ -57,7 +61,7 @@ def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw
     :param grid_connected_hopp: ``bool``,
         Flag for on-grid operation. Enables buying/selling of energy to grid.
     :returns: 
-    
+
     :param hybrid_plant: :class: `hybrid.hybrid_simulation.HybridSimulation`,
         Base class for simulation a Hybrid Plant
     :param combined_pv_wind_power_production_hopp: ``list``,
@@ -83,7 +87,8 @@ def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw
         dispatch_options = {'battery_dispatch': 'heuristic'}
     else:
         dispatch_options = {}
-    hybrid_plant = HybridSimulation(technologies, site, interconnect_kw=interconnection_size_mw * 1e3, dispatch_options=dispatch_options)
+    hybrid_plant = HybridSimulation(
+        technologies, site, interconnect_kw=interconnection_size_mw * 1e3, dispatch_options=dispatch_options)
     hybrid_plant.setup_cost_calculator(create_cost_calculator(interconnection_size_mw,
                                                               bos_cost_source='CostPerMW',
                                                               wind_installed_cost_mw=wind_cost_kw * 1000,
@@ -91,47 +96,57 @@ def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw
                                                               storage_installed_cost_mw=storage_cost_kw * 1000,
                                                               storage_installed_cost_mwh=storage_cost_kwh * 1000
                                                               ))
-    
-
-    hybrid_plant.set_om_costs_per_kw(pv_om_per_kw=None, wind_om_per_kw=wind_om_cost_kw, hybrid_om_per_kw=None)
+   
+    hybrid_plant.set_om_costs_per_kw(
+        pv_om_per_kw=solar_om_cost_kw, wind_om_per_kw=wind_om_cost_kw, hybrid_om_per_kw=None)
     if solar_size_mw > 0:
-        hybrid_plant.pv._financial_model.FinancialParameters.analysis_period = scenario['Useful Life']
-        hybrid_plant.pv._financial_model.FinancialParameters.debt_percent = scenario['Debt Equity']
+        hybrid_plant.pv._financial_model.FinancialParameters.analysis_period = scenario[
+            'Useful Life']
+        hybrid_plant.pv._financial_model.FinancialParameters.debt_percent = scenario[
+            'Debt Equity']
         # hybrid_plant.pv.system_capacity_kw = solar_size_mw * 1000
         # if scenario['ITC Available']:
         #     hybrid_plant.pv._financial_model.TaxCreditIncentives.itc_fed_percent = 26
         # else:
         #     hybrid_plant.pv._financial_model.TaxCreditIncentives.itc_fed_percent = 0
-
+    
     if 'wind' in technologies:
         # hybrid_plant.wind._system_model.Turbine.wind_resource_shear = 0.33
         # hybrid_plant.wind.wake_model = 3
         # hybrid_plant.wind.value("wake_int_loss", 3)
-        hybrid_plant.wind._financial_model.FinancialParameters.analysis_period = scenario['Useful Life']
+        hybrid_plant.wind._financial_model.FinancialParameters.analysis_period = scenario[
+            'Useful Life']
         hybrid_plant.wind._financial_model.FinancialParameters.system_capacity = wind_size_mw * 1000
-        # hybrid_plant.wind.om_capacity = 
-        hybrid_plant.wind._financial_model.FinancialParameters.debt_percent = scenario['Debt Equity']
+        # hybrid_plant.wind.om_capacity =
+        hybrid_plant.wind._financial_model.FinancialParameters.debt_percent = scenario[
+            'Debt Equity']
         hybrid_plant.wind._financial_model.value("debt_option", 0)
-        hybrid_plant.wind._financial_model.FinancialParameters.debt_percent = scenario['Debt Equity']
+        hybrid_plant.wind._financial_model.FinancialParameters.debt_percent = scenario[
+            'Debt Equity']
         hybrid_plant.wind._financial_model.value("debt_option", 0)
         ptc_val = scenario['Wind PTC']
 
         interim_list = list(
             hybrid_plant.wind._financial_model.TaxCreditIncentives.ptc_fed_amount)
         interim_list[0] = ptc_val
-        hybrid_plant.wind._financial_model.TaxCreditIncentives.ptc_fed_amount = tuple(interim_list)
+        hybrid_plant.wind._financial_model.TaxCreditIncentives.ptc_fed_amount = tuple(
+            interim_list)
         #hybrid_plant.wind._system_model.Turbine.wind_turbine_hub_ht = scenario['Tower Height']
 
-        hybrid_plant.wind._financial_model.TaxCreditIncentives.itc_fed_percent = scenario['Wind ITC']
+        hybrid_plant.wind._financial_model.TaxCreditIncentives.itc_fed_percent = [
+            0]  # scenario['Wind ITC']
         hybrid_plant.wind._financial_model.FinancialParameters.real_discount_rate = 7
     if custom_powercurve:
         if turbine_parent_path == None:
             turbine_parent_path = os.path.abspath(os.path.dirname(__file__))
-        powercurve_file = open(os.path.join(turbine_parent_path, scenario['Powercurve File']))
-        powercurve_file_extension = pathlib.Path(os.path.join(turbine_parent_path, scenario['Powercurve File'])).suffix
+        powercurve_file = open(os.path.join(
+            turbine_parent_path, scenario['Powercurve File']))
+        powercurve_file_extension = pathlib.Path(os.path.join(
+            turbine_parent_path, scenario['Powercurve File'])).suffix
         if powercurve_file_extension == '.csv':
-            curve_data = pd.read_csv(os.path.join(turbine_parent_path, scenario['Powercurve File']))            
-            wind_speed = curve_data['Wind Speed [m/s]'].values.tolist() 
+            curve_data = pd.read_csv(os.path.join(
+                turbine_parent_path, scenario['Powercurve File']))
+            wind_speed = curve_data['Wind Speed [m/s]'].values.tolist()
             curve_power = curve_data['Power [kW]']
             hybrid_plant.wind._system_model.Turbine.wind_turbine_powercurve_windspeeds = wind_speed
             hybrid_plant.wind._system_model.Turbine.wind_turbine_powercurve_powerout = curve_power
@@ -144,20 +159,21 @@ def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw
             hybrid_plant.wind._system_model.Turbine.wind_turbine_powercurve_powerout = \
                 powercurve_data['turbine_powercurve_specification']['turbine_power_output']
 
-    
     hybrid_plant.wind.system_capacity_by_num_turbines(wind_size_mw * 1000)
     hybrid_plant.ppa_price = ppa_price
     hybrid_plant.simulate(scenario['Useful Life'])
 
     # HOPP Specific Energy Metrics
     ### wind wind losses, for wind only farms ###
-    combined_pv_wind_power_production_hopp = hybrid_plant.grid._system_model.Outputs.system_pre_interconnect_kwac[0:8759] 
+    combined_pv_wind_power_production_hopp = hybrid_plant.grid._system_model.Outputs.system_pre_interconnect_kwac[
+        0:8759]
     energy_shortfall_hopp = [x - y for x, y in
-                             zip(load,combined_pv_wind_power_production_hopp)]
+                             zip(load, combined_pv_wind_power_production_hopp)]
     energy_shortfall_hopp = [x if x > 0 else 0 for x in energy_shortfall_hopp]
     combined_pv_wind_curtailment_hopp = [x - y for x, y in
-                             zip(combined_pv_wind_power_production_hopp,load)]
-    combined_pv_wind_curtailment_hopp = [x if x > 0 else 0 for x in combined_pv_wind_curtailment_hopp]
+                                         zip(combined_pv_wind_power_production_hopp, load)]
+    combined_pv_wind_curtailment_hopp = [
+        x if x > 0 else 0 for x in combined_pv_wind_curtailment_hopp]
 
     # super simple dispatch battery model with no forecasting TODO: add forecasting
     # print("Length of 'energy_shortfall_hopp is {}".format(len(energy_shortfall_hopp)))
@@ -167,7 +183,8 @@ def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw
 
     # Save the outputs
     annual_energies = hybrid_plant.annual_energies
-    wind_plus_solar_npv = hybrid_plant.net_present_values.wind + hybrid_plant.net_present_values.pv
+    wind_plus_solar_npv = hybrid_plant.net_present_values.wind + \
+        hybrid_plant.net_present_values.pv
     npvs = hybrid_plant.net_present_values
     lcoe = hybrid_plant.lcoe_real.hybrid
     lcoe_nom = hybrid_plant.lcoe_nom.hybrid
@@ -176,5 +193,5 @@ def hopp_for_h2_floris(site, scenario, technologies, wind_size_mw, solar_size_mw
     # print('discount rate', hybrid_plant.wind._financial_model.FinancialParameters.real_discount_rate)
 
     return hybrid_plant, combined_pv_wind_power_production_hopp, combined_pv_wind_curtailment_hopp, \
-           energy_shortfall_hopp,\
-           annual_energies, wind_plus_solar_npv, npvs, lcoe, lcoe_nom
+        energy_shortfall_hopp,\
+        annual_energies, wind_plus_solar_npv, npvs, lcoe, lcoe_nom
