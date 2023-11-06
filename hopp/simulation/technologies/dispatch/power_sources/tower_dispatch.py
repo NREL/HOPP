028d73a1f15a730b0e5594f5442e5fae42751fc6
kbrunik <kbrunik@gmail.com>
2023-11-06 13:24:29 -0800

    Updated dispatch models to use

--- hopp/simulation/technologies/dispatch/power_sources/tower_dispatch.py
+++ hopp/simulation/technologies/dispatch/power_sources/tower_dispatch.py
@@ -1,14 +1,13 @@
 import datetime
 from pyomo.environ import ConcreteModel, Set
 
-import PySAM.Singleowner as Singleowner
-
+from hopp.simulation.technologies.financial import FinancialModelType
 from hopp.simulation.technologies.dispatch.power_sources.csp_dispatch import CspDispatch
 
 
 class TowerDispatch(CspDispatch):
     _system_model: None
-    _financial_model: Singleowner.Singleowner
+    _financial_model: FinancialModelType
     """
 
     """
@@ -16,7 +15,7 @@ class TowerDispatch(CspDispatch):
                  pyomo_model: ConcreteModel,
                  indexed_set: Set,
                  system_model: None,
-                 financial_model: Singleowner.Singleowner,
+                 financial_model: FinancialModelType,
                  block_set_name: str = 'tower'):
         super().__init__(pyomo_model, indexed_set, system_model, financial_model, block_set_name=block_set_name)
 
--- hopp/simulation/technologies/dispatch/power_sources/wind_dispatch.py
+++ hopp/simulation/technologies/dispatch/power_sources/wind_dispatch.py
@@ -1,22 +1,24 @@
+from typing import Union
 from pyomo.environ import ConcreteModel, Set
 
 import PySAM.Windpower as Windpower
-import PySAM.Singleowner as Singleowner
+from hopp.simulation.technologies.wind.floris import Floris
 
+from hopp.simulation.technologies.financial import FinancialModelType
 from hopp.simulation.technologies.dispatch.power_sources.power_source_dispatch import PowerSourceDispatch
 
 
 class WindDispatch(PowerSourceDispatch):
-    _system_model: Windpower.Windpower
-    _financial_model: Singleowner.Singleowner
+    _system_model: Union[Windpower.Windpower,Floris]
+    _financial_model: FinancialModelType
     """
 
     """
     def __init__(self,
                  pyomo_model: ConcreteModel,
                  indexed_set: Set,
-                 system_model: Windpower.Windpower,
-                 financial_model: Singleowner.Singleowner,
+                 system_model: Union[Windpower.Windpower,Floris],
+                 financial_model: FinancialModelType,
                  block_set_name: str = 'wind'):
         super().__init__(pyomo_model, indexed_set, system_model, financial_model, block_set_name=block_set_name)
 
