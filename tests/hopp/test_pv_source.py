5ac1d3651981451fab04e90d6e4a21aa8484df68
kbrunik <kbrunik@gmail.com>
2024-02-07 10:49:19 -0800

    add battery size and mass

--- tests/hopp/test_pv_source.py
+++ tests/hopp/test_pv_source.py
@@ -68,7 +68,7 @@ def test_pv_plant_area(site, subtests):
     pv_plant = PVPlant(site=site, config=config)
     
     with subtests.test("plant area"):
-        assert pv_plant.plant_area == pytest.approx(457.94, 0.1)
+        assert pv_plant.system_area == pytest.approx(457.94, 0.1)
 
 def test_pv_plant_mass(site, subtests):
     system_capacity_kw = 100.0
@@ -78,4 +78,4 @@ def test_pv_plant_mass(site, subtests):
     pv_plant = PVPlant(site=site, config=config)
 
     with subtests.test("plant mass"):
-        assert pv_plant.plant_mass == pytest.approx(5079.51,0.01)
+        assert pv_plant.system_mass == pytest.approx(5079.51,0.01)
