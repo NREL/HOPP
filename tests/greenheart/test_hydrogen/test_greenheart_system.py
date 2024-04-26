6770d192610a5f4c81b147f7dc9d96533bc06dee
Jared Thomas <jaredthomas68@gmail.com>
2024-04-26 17:00:59 -0600

    add summary for PoseOptimization class

--- tests/greenheart/test_hydrogen/test_greenheart_system.py
+++ tests/greenheart/test_hydrogen/test_greenheart_system.py
@@ -202,19 +202,23 @@ def test_simulation_wind_wave_solar_battery(subtests):
         # TODO base this test value on something. Currently just based on output at writing.
         assert lcoe == approx(0.12936583137325117, rel=rtol)  
 
+    with subtests.test("no conflict in om cost does not raise warning"):
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")
+
     with subtests.test("wind_om_per_kw conflict raise warning"):
         config.hopp_config["technologies"]["wind"]["fin_model"]["system_costs"]["om_fixed"][0] = 1.0
-        with warns(UserWarning, match=f"The 'om_fixed' value in the wind `fin_model`"):
+        with warns(UserWarning, match=f"The 'om_fixed' value in the wind 'fin_model'"):
             lcoe, lcoh, _, hi = run_simulation(config)
     
     with subtests.test("pv_om_per_kw conflict raise warning"):
         config.hopp_config["technologies"]["pv"]["fin_model"]["system_costs"]["om_fixed"][0] = 1.0
-        with warns(UserWarning, match=f"The 'om_fixed' value in the pv `fin_model`"):
+        with warns(UserWarning, match=f"The 'om_fixed' value in the pv 'fin_model'"):
             lcoe, lcoh, _, hi = run_simulation(config)
 
     with subtests.test("battery_om_per_kw conflict raise warning"):
         config.hopp_config["technologies"]["battery"]["fin_model"]["system_costs"]["om_batt_fixed_cost"] = 1.0
-        with warns(UserWarning, match=f"The 'om_batt_fixed_cost' value in the battery `fin_model`"):
+        with warns(UserWarning, match=f"The 'om_batt_fixed_cost' value in the battery 'fin_model'"):
             lcoe, lcoh, _, hi = run_simulation(config)
 
 def test_simulation_wind_onshore(subtests):
@@ -356,7 +360,7 @@ def test_simulation_wind_battery_pv_onshore_steel_ammonia(subtests):
         assert ammonia_finance.sol.get("price") == approx(lcoa_expected, rel=rtol)
 
 def test_utilities(subtests):
-    with subtests.test("`visualize_plant()` only works with the 'floris' wind model"):
+    with subtests.test("'visualize_plant()' only works with the 'floris' wind model"):
         hopp_config ={"technologies": {"wind": {"model_name": "pysam"}}}
         with raises(NotImplementedError, match="only works with the 'floris' wind model"):
             visualize_plant(hopp_config, None, None, None, None, None, None, None, None, None, None, None)
