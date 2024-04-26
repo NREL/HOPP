b65d3f71dabd784c60e3d489966dcf1507775f87
Jared Thomas <jaredthomas68@gmail.com>
2024-04-26 09:56:49 -0600

    seperate greenheart utilities tests by function

--- tests/greenheart/test_greenheart_utilities.py
+++ tests/greenheart/test_greenheart_utilities.py
@@ -1,11 +1,16 @@
 from pytest import raises
 from greenheart.tools.eco.utilities import ceildiv, visualize_plant
 
-def test_utilities(subtests):
+
+def test_visualize_plant(subtests):
+    
     with subtests.test("`visualize_plant()` only works with the 'floris' wind model"):
         hopp_config ={"technologies": {"wind": {"model_name": "pysam"}}}
         with raises(NotImplementedError, match="only works with the 'floris' wind model"):
             visualize_plant(hopp_config, None, None, None, None, None, None, None, None, None, None, None)
+
+def test_ceildiv(subtests):
+    
     with subtests.test("ceildiv"):
         a = 8
         b = 3
