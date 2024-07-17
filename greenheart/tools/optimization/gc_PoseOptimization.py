8c5ac2541ff317d67ebe47a4e3cbd148ae3db42d
Jared Thomas <jaredthomas68@gmail.com>
2024-07-17 15:19:22 -0700

    set input defautls for battery

--- greenheart/tools/optimization/gc_PoseOptimization.py
+++ greenheart/tools/optimization/gc_PoseOptimization.py
@@ -416,7 +416,10 @@ class PoseOptimization(object):
                 lower = self.config.greenheart_config["opt_options"]["constraints"]["battery_discharge_time"]["lower"]
                 upper = self.config.greenheart_config["opt_options"]["constraints"]["battery_discharge_time"]["upper"]
                 opt_prob.model.add_subsystem("con_battery_discharge_time", subsys=om.ExecComp(['battery_discharge_time=battery_capacity_kwh/battery_capacity_kw']), promotes=["*"])
+                opt_prob.model.set_input_defaults('battery_capacity_kw', units="kW", val=0.0)
+                opt_prob.model.set_input_defaults('battery_capacity_kwh', units="kW*h", val=0.0)
                 opt_prob.model.add_constraint("battery_discharge_time", lower=lower, upper=upper)
+
 
         # User constraints
         user_constr = self.config.greenheart_config["opt_options"]["constraints"]["user"]
