22ebb5f25ab4bc2193439a3334c5fd69c20e4dbc
kbrunik <kbrunik@gmail.com>
2024-08-06 09:47:44 -0500

    update default OM

--- tests/greenheart/test_greenheart_system.py
+++ tests/greenheart/test_greenheart_system.py
@@ -197,30 +197,30 @@ def test_simulation_wind_wave_solar_battery(subtests):
 
     with subtests.test("lcoh"):
         # TODO base this test value on something. Currently just based on output at writing.
-        assert results.lcoh == approx(17.102809848001787, rel=rtol)
+        assert results.lcoh == approx(17.105851423463314, rel=rtol)
 
     # TODO base this test value on something. Currently just based on output at writing.
     with subtests.test("lcoe"):
         # TODO base this test value on something. Currently just based on output at writing.
-        assert results.lcoe == approx(0.12936657762567322, rel=rtol)  
+        assert results.lcoe == approx(0.1294193054583137, rel=rtol)  
 
     with subtests.test("no conflict in om cost does not raise warning"):
         with warnings.catch_warnings():
             warnings.simplefilter("error")
 
     with subtests.test("wind_om_per_kw conflict raise warning"):
-        config.hopp_config["technologies"]["wind"]["fin_model"]["system_costs"]["om_fixed"][0] = 1.0
-        with warns(UserWarning, match=f"The 'om_fixed' value in the wind 'fin_model'"):
+        config.hopp_config["technologies"]["wind"]["fin_model"]["system_costs"]["om_capacity"][0] = 1.0
+        with warns(UserWarning, match=f"The 'om_capacity' value in the wind 'fin_model'"):
             _ = run_simulation(config)
     
     with subtests.test("pv_om_per_kw conflict raise warning"):
-        config.hopp_config["technologies"]["pv"]["fin_model"]["system_costs"]["om_fixed"][0] = 1.0
-        with warns(UserWarning, match=f"The 'om_fixed' value in the pv 'fin_model'"):
+        config.hopp_config["technologies"]["pv"]["fin_model"]["system_costs"]["om_capacity"][0] = 1.0
+        with warns(UserWarning, match=f"The 'om_capacity' value in the pv 'fin_model'"):
             _ = run_simulation(config)
 
     with subtests.test("battery_om_per_kw conflict raise warning"):
-        config.hopp_config["technologies"]["battery"]["fin_model"]["system_costs"]["om_batt_fixed_cost"] = 1.0
-        with warns(UserWarning, match=f"The 'om_batt_fixed_cost' value in the battery 'fin_model'"):
+        config.hopp_config["technologies"]["battery"]["fin_model"]["system_costs"]["om_capacity"][0] = 1.0
+        with warns(UserWarning, match=f"The 'om_capacity' value in the battery 'fin_model'"):
             _ = run_simulation(config)
 
 def test_simulation_wind_onshore(subtests):
@@ -343,15 +343,15 @@ def test_simulation_wind_battery_pv_onshore_steel_ammonia(subtests):
 
     # TODO base this test value on something
     with subtests.test("lcoh"):
-        assert greenheart_output.lcoh == approx(3.1476029100059995, rel=rtol)
+        assert greenheart_output.lcoh == approx(3.1509756450008752, rel=rtol)
 
     # TODO base this test value on something
     with subtests.test("lcoe"):
-        assert greenheart_output.lcoe == approx(0.03470063589149857, rel=rtol)
+        assert greenheart_output.lcoe == approx(0.03476011434910082, rel=rtol)
 
     # TODO base this test value on something
     with subtests.test("steel_finance"):
-        lcos_expected = 1349.11151601181
+        lcos_expected = 1349.3364242679354
 
         assert greenheart_output.steel_finance.sol.get("price") == approx(lcos_expected, rel=rtol)
 
