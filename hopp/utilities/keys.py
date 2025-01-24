30bb7ae42750c281d21032a887759285223fed30
John Jasa <johnjasa11@gmail.com>
2025-01-24 11:56:50 -0700

    Fixing keys.py

--- hopp/utilities/keys.py
+++ hopp/utilities/keys.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv, find_dotenv
 import os
 
-developer_nrel_gov_key = "EIJvR91RkJEA2dSelHTnoCux22tPncYScLs8kqde"
-developer_nrel_gov_email = "john.jasa@nrel.gov"
+developer_nrel_gov_key = ""
+developer_nrel_gov_email = ""
 
 
 def set_developer_nrel_gov_key(key: str):
