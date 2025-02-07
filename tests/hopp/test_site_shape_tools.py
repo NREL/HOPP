a3e52b7c001052546087b9043a4eec57b32dcea3
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-02-07 15:27:30 -0700

    cleaned up tests for site_shape_tools and added tests for rotating site

--- tests/hopp/test_site_shape_tools.py
+++ tests/hopp/test_site_shape_tools.py
@@ -1,79 +1,62 @@
 from pytest import approx
 from shapely.geometry import Polygon
 import hopp.simulation.technologies.sites.site_shape_tools as shape_tools
+import numpy as np
 
 def test_circle_area():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    polygon, vertices = shape_tools.make_circle(area_m2, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_circle(area_m2)
     assert polygon.area == approx(area_m2,rel = 0.1)
 
 def test_square_area():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    polygon, vertices = shape_tools.make_square(area_m2, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_square(area_m2)
     assert polygon.area == approx(area_m2,rel = 0.1)
 
 def test_rectangle_area():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    aspect_ratio = 1.5
-    polygon, vertices = shape_tools.make_rectangle(area_m2, aspect_ratio = aspect_ratio, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_rectangle(area_m2)
     assert polygon.area == approx(area_m2,rel = 0.1)
 
 def test_hexagon_area():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    polygon, vertices = shape_tools.make_hexagon(area_m2, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_hexagon(area_m2)
     assert polygon.area == approx(area_m2,rel = 0.1)
 
 def test_circle_vertices_default():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    polygon, vertices = shape_tools.make_circle(area_m2, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_circle(area_m2)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert min(x_verts) == 0.0
+    assert min(y_verts) == 0.0
 
 def test_square_vertices_default():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    polygon, vertices = shape_tools.make_square(area_m2, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_square(area_m2)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
     assert len(vertices)==4
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert min(x_verts) == 0.0
+    assert min(y_verts) == 0.0
 
 def test_rectangle_vertices_default():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    aspect_ratio = 1.5
-    polygon, vertices = shape_tools.make_rectangle(area_m2, aspect_ratio = aspect_ratio, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_rectangle(area_m2)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
     assert len(vertices)==4
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert min(x_verts) == 0.0
+    assert min(y_verts) == 0.0
 
 def test_hexagon_vertices_default():
     area_m2 = 1e3
-    x0 = 0.0
-    y0 = 0.0
-    polygon, vertices = shape_tools.make_hexagon(area_m2, x0 = x0, y0 = y0)
+    polygon, vertices = shape_tools.make_hexagon(area_m2)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
     assert len(vertices)==6
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert min(x_verts) == 0.0
+    assert min(y_verts) == 0.0
 
 
 def test_circle_vertices_offset():
@@ -83,8 +66,8 @@ def test_circle_vertices_offset():
     polygon, vertices = shape_tools.make_circle(area_m2, x0 = x0, y0 = y0)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert min(x_verts) == x0
+    assert min(y_verts) == y0
 
 def test_square_vertices_offset():
     area_m2 = 1e3
@@ -93,9 +76,9 @@ def test_square_vertices_offset():
     polygon, vertices = shape_tools.make_square(area_m2, x0 = x0, y0 = y0)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
-    assert len(vertices)==4
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert len(vertices) == 4
+    assert min(x_verts) == x0
+    assert min(y_verts) == y0
 
 def test_rectangle_vertices_offset():
     area_m2 = 1e3
@@ -105,9 +88,9 @@ def test_rectangle_vertices_offset():
     polygon, vertices = shape_tools.make_rectangle(area_m2, aspect_ratio = aspect_ratio, x0 = x0, y0 = y0)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
-    assert len(vertices)==4
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert len(vertices) == 4
+    assert min(x_verts) == x0
+    assert min(y_verts) == y0
 
 def test_hexagon_vertices_offset():
     area_m2 = 1e3
@@ -116,9 +99,9 @@ def test_hexagon_vertices_offset():
     polygon, vertices = shape_tools.make_hexagon(area_m2, x0 = x0, y0 = y0)
     x_verts = [v[0] for v in vertices]
     y_verts = [v[1] for v in vertices]
-    assert len(vertices)==6
-    assert min(x_verts)==x0
-    assert min(y_verts)==y0
+    assert len(vertices) == 6
+    assert min(x_verts) == x0
+    assert min(y_verts) == y0
 
 def test_distance_between_points():
     x0 = 0.0
@@ -136,12 +119,36 @@ def test_angle_between_points():
     x1 = 1.0
     y1 = 1.0
     angle = shape_tools.calc_angle_between_two_points_cartesian(x0,y0,x1,y1)
-    assert angle==45.0
+    assert angle == 45.0
 
 def test_sort_site_verts():
     invalid_x_points = [0.0,5.0,0.0,5.0]
     invalid_y_points = [0.0,0.0,5.0,5.0]
     invalid_verts = [[x,y] for x,y in zip(invalid_x_points,invalid_y_points)]
+    invalid_verts = np.array(invalid_verts)
     valid_verts = shape_tools.check_site_verts(invalid_verts)
     valid_shape = Polygon(valid_verts)
     assert valid_shape.area == 25.0
+
+def test_rotate_site_center_area():
+    area_m2 = 1e3
+    rotation_angle = 45
+    polygon_original, vertices_original = shape_tools.make_hexagon(area_m2)
+    rotated_polygon, rotated_vertices = shape_tools.rotate_shape(
+        site_polygon = polygon_original, 
+        rotation_angle_deg = rotation_angle
+    )
+    assert round(rotated_polygon.centroid.x,3) == round(polygon_original.centroid.x,3)
+    assert round(rotated_polygon.centroid.y,3) == round(polygon_original.centroid.y,3)
+    assert rotated_polygon.area == polygon_original.area
+
+def test_rotate_site_vertices():
+    area_m2 = 1e3
+    rotation_angle = 45
+    polygon_original, vertices_original = shape_tools.make_square(area_m2)
+    rotated_polygon, rotated_vertices = shape_tools.rotate_shape(
+        site_polygon = polygon_original, 
+        rotation_angle_deg = rotation_angle
+    )
+    assert round(rotated_polygon.exterior.xy[0][0],3) == round(polygon_original.centroid.x,3)
+    assert round(rotated_polygon.exterior.xy[1][1],3) == round(polygon_original.centroid.y,3)
