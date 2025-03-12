effd8814fb0170ba79ecb7712461aafc1ac1a341
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-03-12 17:06:12 -0600

    removed tests for removed function in turbine models interface test

--- tests/hopp/test_turbine_models_interface.py
+++ tests/hopp/test_turbine_models_interface.py
@@ -5,7 +5,7 @@ import json
 from hopp.simulation.technologies.wind.floris import Floris
 from hopp.utilities import load_yaml
 from hopp.simulation.technologies.wind.wind_plant import WindPlant, WindConfig
-from hopp.tools.design.wind.turbine_library_tools import check_turbine_name, check_turbine_library_for_turbine
+from hopp.tools.design.wind.turbine_library_tools import check_turbine_library_for_turbine
 from hopp.simulation.technologies.sites.site_info import SiteInfo
 from hopp import ROOT_DIR
 from hopp.simulation import HoppInterface
@@ -37,29 +37,18 @@ def site_input():
 def test_turbine_library_tools_for_valid_turbine_name(subtests):
     valid_turbine_name = "BergeyExcel15_15.6kW_9.6"
     is_valid = check_turbine_library_for_turbine(valid_turbine_name)
-    res_valid_name = check_turbine_name(valid_turbine_name)
     
     with subtests.test("valid name (bool)"):
         assert is_valid is True
 
-    with subtests.test("valid name (str)"):
-        assert res_valid_name == valid_turbine_name
 
 def test_turbine_library_tools_for_invalid_turbine_name(subtests):
     
     invalid_turbine_name_close_match = "BergeyExcel15"
     is_valid = check_turbine_library_for_turbine(invalid_turbine_name_close_match)
-    res_invalid_name = check_turbine_name(invalid_turbine_name_close_match)
-
+    
     with subtests.test("invalid name (bool)"): 
         assert is_valid is False
-
-    with subtests.test("invalid name (str)"):     
-        assert res_invalid_name != invalid_turbine_name_close_match
-    
-    with subtests.test("invalid name best match"):
-        assert res_invalid_name == "BergeyExcel15_15.6kW_9.6"
-
     
 def test_floris_nrel_5mw(site_input,subtests):
     floris_template = load_yaml(str(FLORIS_V4_TEMPLATE_PATH))
