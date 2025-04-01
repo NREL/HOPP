3995c32e97ea2e1b8143c76184feecebbb596328
Jared Thomas <jaredthomas68@gmail.com>
2025-04-01 10:30:55 -0600

    include subtests

--- tests/hopp/test_battery_dispatch.py
+++ tests/hopp/test_battery_dispatch.py
@@ -55,7 +55,7 @@ def create_test_objective_rule(m):
                 for t in m.battery.index_set())
 
 
-def test_batterystateless_dispatch():
+def test_batterystateless_dispatch(subtests):
     expected_objective = 28957.15
 
     # Run battery stateful as system model first
@@ -88,21 +88,41 @@ def test_batterystateless_dispatch():
     assert_units_consistent(model)
     results = HybridDispatchBuilderSolver.glpk_solve_call(model)
 
-    assert results.solver.termination_condition == TerminationCondition.optimal
-    assert pyomo.value(model.test_objective) == pytest.approx(expected_objective, 1e-5)
+    with subtests.test("TerminationCondition"):
+        assert results.solver.termination_condition == TerminationCondition.optimal
+    
+    with subtests.test("expected_objective"):
+        assert pyomo.value(model.test_objective) == pytest.approx(expected_objective, 1e-5)
 
     sum_charge_power = sum(battery.dispatch.charge_power)
     sum_discharge_power = sum(battery.dispatch.discharge_power)
-    assert sum(battery.dispatch.charge_power) == pytest.approx(sum_charge_power, 1e-2)
-    assert sum(battery.dispatch.discharge_power) == pytest.approx(sum_discharge_power, 1e-2)
+
+    with subtests.test("sum_charge_power"):
+        assert sum(battery.dispatch.charge_power) == pytest.approx(sum_charge_power, 1e-2)
+
+    with subtests.test("sum_discharge_power"):
+        assert sum(battery.dispatch.discharge_power) == pytest.approx(sum_discharge_power, 1e-2)
 
     battery.simulate_with_dispatch(48, 0)
     for i in range(24):
-        dispatch_power = battery.dispatch.power[i] * 1e3
-        assert battery.outputs.P[i] == pytest.approx(dispatch_power, 1e-3 * abs(dispatch_power))
-    assert battery.outputs.dispatch_lifecycles_per_day[0:2] == pytest.approx([0.75048, 1.50096], rel=1e-3)
-    assert battery.outputs.n_cycles[23] == 0
-    assert battery.outputs.n_cycles[47] == 1
+        with subtests.test(f"battery.dispatch.power[{i}]"):
+            dispatch_power = battery.dispatch.power[i] * 1e3
+            assert battery.outputs.P[i] == pytest.approx(dispatch_power*1E-3, rel=1E-3)
+
+    for i in range(24):
+        with subtests.test(f"battery.dispatch.soc[{i}]"):
+            dispatch_soc = battery.dispatch.soc[i] * 1e3
+            # import pdb; pdb.set_trace()
+            assert battery.outputs.SOC[i] == pytest.approx(dispatch_soc*1E-3, rel=1E-3)
+
+    with subtests.test("sum_charge_power"):
+        assert battery.outputs.dispatch_lifecycles_per_day[0:2] == pytest.approx([0.75048, 1.50096], rel=1e-3)
+
+    with subtests.test("sum_charge_power"):
+        assert battery.outputs.n_cycles[23] == 0
+
+    with subtests.test("sum_charge_power"):
+        assert battery.outputs.n_cycles[47] == 1
 
     # Run battery stateless as system model to compare
     technologies['battery']['tracking'] = False
@@ -132,30 +152,46 @@ def test_batterystateless_dispatch():
     assert_units_consistent(model_sl)
     results = HybridDispatchBuilderSolver.glpk_solve_call(model_sl)
 
-    assert results.solver.termination_condition == TerminationCondition.optimal
-    assert pyomo.value(model_sl.test_objective) == pytest.approx(expected_objective, 1e-5)
+    with subtests.test("sum_charge_power"):
+        assert results.solver.termination_condition == TerminationCondition.optimal
+    
+    with subtests.test("sum_charge_power"):
+        assert pyomo.value(model_sl.test_objective) == pytest.approx(expected_objective, 1e-5)
 
-    assert sum(battery_sl.dispatch.charge_power) == pytest.approx(sum_charge_power, 1e-2)
-    assert sum(battery_sl.dispatch.discharge_power) == pytest.approx(sum_discharge_power, 1e-2)
+    with subtests.test("sum_charge_power"):
+        assert sum(battery_sl.dispatch.charge_power) == pytest.approx(sum_charge_power, 1e-2)
+    
+    with subtests.test("sum_charge_power"):
+        assert sum(battery_sl.dispatch.discharge_power) == pytest.approx(sum_discharge_power, 1e-2)
 
     battery_sl.simulate_with_dispatch(48, 0)
     for i in range(24):
-        dispatch_power = battery_sl.dispatch.power[i] * 1e3
-        assert battery_sl.outputs.P[i] == pytest.approx(dispatch_power, 1e-3 * abs(dispatch_power))
+        with subtests.test(f"battery_sl.dispatch.power[{i}]"):
+            dispatch_power = battery_sl.dispatch.power[i] * 1e3
+            assert battery_sl.outputs.P[i] == pytest.approx(dispatch_power, 1e-3 * abs(dispatch_power))
 
     battery_dispatch = np.array(battery.dispatch.power)[0:48]
     battery_actual = np.array(battery.generation_profile[0:dispatch_n_look_ahead]) * 1e-3   # convert to MWh
     battery_sl_dispatch = np.array(battery_sl.dispatch.power)[0:48]
     battery_sl_actual = np.array(battery_sl.generation_profile)[0:48] * 1e-3   # convert to MWh
 
-    assert sum(battery_dispatch - battery_sl_dispatch) == 0
-    assert sum(abs(battery_actual - battery_dispatch)) <= 33
-    assert sum(abs(battery_sl_actual - battery_sl_dispatch)) == 0
-    assert sum(abs(battery_actual - battery_sl_actual)) <= 33
-    assert battery_sl.outputs.lifecycles_per_day[0:2] == pytest.approx([0.75048, 1.50096], rel=1e-3)
+    with subtests.test("battery_dispatch vs battery_sl_dispatch"):
+        assert sum(battery_dispatch - battery_sl_dispatch) == 0
+    
+    with subtests.test("battery_actual vs battery_dispatch"):
+        assert sum(abs(battery_actual - battery_dispatch)) <= 33
+    
+    with subtests.test("battery_sl_actual vs battery_sl_dispatch"):
+        assert sum(abs(battery_sl_actual - battery_sl_dispatch)) == 0
+    
+    with subtests.test("battery_actual vs battery_sl_actual"):
+        assert sum(abs(battery_actual - battery_sl_actual)) <= 33
+    
+    with subtests.test("lifecycles_per_day"):
+        assert battery_sl.outputs.lifecycles_per_day[0:2] == pytest.approx([0.75048, 1.50096], rel=1e-3)
 
 
-def test_batterystateless_cycle_limits():
+def test_batterystateless_cycle_limits(subtests):
     expected_objective = 22513      # objective is less than above due to cycling limits
     
     technologies = technologies_input.copy()
@@ -186,10 +222,15 @@ def test_batterystateless_cycle_limits():
     assert_units_consistent(model_sl)
     results = HybridDispatchBuilderSolver.glpk_solve_call(model_sl)
 
-    assert results.solver.termination_condition == TerminationCondition.optimal
-    assert pyomo.value(model_sl.test_objective) == pytest.approx(expected_objective, 1e-3)
+    
+    with subtests.test("termination_condition"):
+        assert results.solver.termination_condition == TerminationCondition.optimal
+    
+    with subtests.test("test_objective"):
+        assert pyomo.value(model_sl.test_objective) == pytest.approx(expected_objective, 1e-3)
 
     battery_sl.simulate_with_dispatch(48, 0)
 
-    assert battery_sl.outputs.lifecycles_per_day[0:2] == pytest.approx([0.75048, 1], rel=1e-3)
+    with subtests.test("lifecycles_per_day"):
+        assert battery_sl.outputs.lifecycles_per_day[0:2] == pytest.approx([0.75048, 1], rel=1e-3)
 
