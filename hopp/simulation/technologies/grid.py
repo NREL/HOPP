ca02a6db22ed218f4716596ae3b62fcfd740a690
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-04-10 11:56:17 -0600

    updated generation_profile_wo_battery setter

--- hopp/simulation/technologies/grid.py
+++ hopp/simulation/technologies/grid.py
@@ -276,12 +276,11 @@ class Grid(PowerSource):
     @property
     def generation_profile_wo_battery(self) -> Sequence:
         """System power generated without battery [kW]"""
-        # if 'gen_without_battery' in self._financial_model.SystemOutput.export().keys():
-        #     return self._financial_model.value('gen_without_battery')
-        return self._system_model.SystemOutput.gen
+        return self._financial_model.value('gen_without_battery')
 
     @generation_profile_wo_battery.setter
     def generation_profile_wo_battery(self, system_generation_wo_battery_kw: Sequence):
+        self._financial_model.value('gen_without_battery',system_generation_wo_battery_kw)
         self._system_model.SystemOutput.gen = system_generation_wo_battery_kw
 
     @property
