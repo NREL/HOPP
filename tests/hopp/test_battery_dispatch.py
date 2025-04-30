0e005cd6b9c7b3b9585ab450806ed4ed901c9605
Jared Thomas <jaredthomas68@gmail.com>
2025-04-30 11:27:17 -0600

    adjust test values

--- tests/hopp/test_battery_dispatch.py
+++ tests/hopp/test_battery_dispatch.py
@@ -230,13 +230,13 @@ def test_batterystateless_dispatch(subtests):
         assert sum(battery_dispatch - battery_sl_dispatch) == pytest.approx(0.0)
     
     with subtests.test("battery_actual vs battery_dispatch"):
-        assert sum(abs(battery_actual - battery_dispatch)) <= 33.5
+        assert sum(abs(battery_actual - battery_dispatch)) <= 33.9
     
     with subtests.test("battery_sl_actual vs battery_sl_dispatch"):
         assert sum(abs(battery_sl_actual - battery_sl_dispatch)) == pytest.approx(0.0)
     
     with subtests.test("battery_actual vs battery_sl_actual"):
-        assert sum(abs(battery_actual - battery_sl_actual)) <= 33.5
+        assert sum(abs(battery_actual - battery_sl_actual)) <= 33.9
     
     with subtests.test("lifecycles_per_day"):
         assert battery_sl.outputs.lifecycles_per_day[0:2] == pytest.approx([0.75048, 1.50096], rel=1e-3)
